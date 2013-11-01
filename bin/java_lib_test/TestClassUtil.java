31c19be5058ef4b7bbdbc9eddc12cf586778fd0a
behram mistree <bmistree@stanford.edu>
2013-11-01 11:43:31 -0700

    Added a method in test utilities to generate single sided endpoints for tests.

--- bin/java_lib_test/TestClassUtil.java
+++ bin/java_lib_test/TestClassUtil.java
@@ -1,8 +1,17 @@
 package java_lib_test;
 
+import ralph.LockedObject;
+import ralph.VariableStore;
+import ralph.LockedVariables.LockedNumberVariable;
+import ralph.RalphGlobals;
+import ralph.Endpoint;
+import RalphConnObj.SingleSideConnection;
 
 public class TestClassUtil
 {
+    public static final String NUM_TVAR_NAME = "num_tvar";
+    public static final Double NUM_TVAR_INIT_VAL = new Double (5);
+    
     public static void print_success(String test_name)
     {
         System.out.println("Test " + test_name + " .....");
@@ -13,4 +22,33 @@ public class TestClassUtil
         System.out.println("Test " + test_name + " .....");
         System.out.println("    FAILURE\n");
     }
+
+    
+    /**
+       @returns {Endpoint} --- Contains variables in base stack frame
+       with number tvar named NUM_TVAR_NAME and init val
+       NUM_TVAR_INIT_VAL.
+     */
+    public static Endpoint create_default_single_endpoint()
+    {
+        String dummy_host_uuid = "dummy_host_uuid";
+
+        VariableStore vstore = new VariableStore();
+        
+        // adding a number tvar
+        vstore.add_var(
+            NUM_TVAR_NAME,
+            new LockedNumberVariable(
+                dummy_host_uuid,false, NUM_TVAR_INIT_VAL));
+
+        Endpoint to_return = new Endpoint(
+            new RalphGlobals(),
+            dummy_host_uuid,
+            new SingleSideConnection(),
+            vstore);
+
+
+        return to_return;
+    }
+    
 }
