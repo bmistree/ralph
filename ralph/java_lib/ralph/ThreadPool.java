a4fbd6262f541a2d8402b7ceb06b3d76dfacdb0f
behram mistree <bmistree@stanford.edu>
2013-11-01 14:16:31 -0700

    Made all threads in thread pool daemons: can create ugly shutdown of threads.  But at least the process terminates.  Should fix to do something cleaner.

--- ralph/java_lib/ralph/ThreadPool.java
+++ ralph/java_lib/ralph/ThreadPool.java
@@ -2,6 +2,7 @@ package ralph;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 import RalphServiceActions.ServiceAction;
 
@@ -11,7 +12,18 @@ public class ThreadPool
 	
     public ThreadPool(int num_threads)
     {
-        executor = Executors.newFixedThreadPool(num_threads);
+        executor = Executors.newFixedThreadPool(
+            num_threads,
+            new ThreadFactory()
+            {
+                // each thread created is a daemon
+                public Thread newThread(Runnable r)
+                {
+                    Thread t=new Thread(r);
+                    t.setDaemon(true);
+                    return t;
+                }
+            });
     }
 	
     public void add_service_action(ServiceAction service_action)
