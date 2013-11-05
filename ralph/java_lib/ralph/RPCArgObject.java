6cd6ecf4aeb0a869f067e1beb7f6c7448ad2740c
behram mistree <bmistree@stanford.edu>
2013-11-05 13:39:31 -0800

    Added a wrapper for an argument object passed into an rpc.  Contain information about whether the argument passed in is a reference or can be copied.

---
+++ ralph/java_lib/ralph/RPCArgObject.java
@@ -0,0 +1,15 @@
+package ralph;
+
+
+public RPCArgObject<T,D>
+{
+    public LockedObject<T,D> arg_to_pass;
+    public boolean is_reference;
+
+    public RPCArgObject(LockedObject<T,D> _arg_to_pass, boolean _is_reference)
+    {
+        arg_to_pass = _arg_to_pass;
+        is_reference = _is_reference;
+    }
+
+}
