9c7c72b5b39a540a442ee9530ae7149d24a746b5
behram mistree <bmistree@stanford.edu>
2013-11-14 13:53:01 -0800

    Added a test case for parallel statements.  Apparently, anonymous parallel blocks cannot be cloned.  I need to change logic.

---
+++ bin/java_lib_test/BasicParallel.java
@@ -0,0 +1,65 @@
+package java_lib_test;
+
+import ralph.LockedVariables.LockedNumberVariable;
+import ralph.Endpoint;
+import ralph.LockedActiveEvent;
+import ralph.ParallelBlock;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.ArrayList;
+
+public class BasicParallel
+{
+    protected static String test_name = "BasicParallel";
+    
+    /**
+       Tests that can run multiple statements in parallel using a
+       ParallelBlock.
+
+       Does so by creating a single atomic int and then loading it
+       multiple times into a list.  After running par_block, should
+       have incremented atomic integer by number of elements in list.
+     */
+    public static void main (String [] args)
+    {
+        if (BasicParallel.run_test())
+            TestClassUtil.print_success(test_name);
+        else
+            TestClassUtil.print_failure(test_name);
+    }
+
+    public static boolean run_test()
+    {
+        int initial_atomic_value = 5;
+        int num_to_parallelize_over = 25;
+        AtomicInteger atom_int = new AtomicInteger(initial_atomic_value);
+        ArrayList<AtomicInteger> atom_int_list = new ArrayList<AtomicInteger>();
+        for (int i=0; i < num_to_parallelize_over; ++i)
+            atom_int_list.add(atom_int);
+        
+        
+        Endpoint endpt = TestClassUtil.create_default_single_endpoint();
+        LockedActiveEvent active_event = null;
+        try
+        {
+            active_event = endpt._act_event_map.create_root_event();
+            new ParallelBlock<AtomicInteger>(
+                endpt.global_var_stack,active_event)
+            {
+                protected void internal_call()
+                {
+                    to_run_on.getAndIncrement();
+                }
+            }.exec_par(atom_int_list);
+        }
+        catch (Exception _ex)
+        {
+            _ex.printStackTrace();
+            return false;
+        }
+
+
+        
+        // test passed
+        return true;
+    }
+}
