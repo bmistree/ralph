52ad48c2fbb88c33ac739c9a2e36142ea44bbab2
bmistree <bmistree@stanford.edu>
2013-11-19 23:25:30 -0800

    Added usage message for emit script.

--- bin/emit_file.py
+++ bin/emit_file.py
@@ -17,11 +17,32 @@ def compile_ralph(input_filename,output_filename,package_name,program_name):
     file_fd.write(emitted_text)
     file_fd.flush()
     file_fd.close()
-    
+
+def print_usage():
+
+    print '''
+
+./emit_file.py <input_filename> <output_filename> <pkg_name> <program_name>
+
+Args:
+
+  input_filename --- The name of a ralph file to compile
+
+  output_filename --- The name of a java file to compile to
+
+  pkg_name --- The package that the ralph program should compile into
+
+  program_name --- The name of the class that wraps endpoint classes.
+
+'''
 
 if __name__ == '__main__':
-    input_filename = sys.argv[1]
-    output_filename = sys.argv[2]
-    package_name = sys.argv[3]
-    program_name = sys.argv[4]
-    compile_ralph(input_filename,output_filename,package_name,program_name)
+
+    if len(sys.argv) != 5:
+        print_usage()
+    else:
+        input_filename = sys.argv[1]
+        output_filename = sys.argv[2]
+        package_name = sys.argv[3]
+        program_name = sys.argv[4]
+        compile_ralph(input_filename,output_filename,package_name,program_name)
