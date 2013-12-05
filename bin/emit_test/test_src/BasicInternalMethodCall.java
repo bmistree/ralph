08b4eff7bf7e7d2e9ee4efe2d6d2cd3d8d3257f3
behram mistree <bmistree@stanford.edu>
2013-12-05 15:18:31 -0800

    Testing internal method calls.

---
+++ bin/emit_test/test_src/BasicInternalMethodCall.java
@@ -0,0 +1,47 @@
+package emit_test_harnesses;
+
+import emit_test_package.InternalMethodCall.InternalMethodCaller;
+import RalphConnObj.SingleSideConnection;
+import ralph.RalphGlobals;
+
+public class BasicInternalMethodCall
+{
+    public static void main(String[] args)
+    {
+        if (BasicInternalMethodCall.run_test())
+            System.out.println("\nSUCCESS in BasicInternalMethodCall\n");
+        else
+            System.out.println("\nFAILURE in BasicInternalMethodCall\n");
+    }
+
+    public static boolean run_test()
+    {
+        try
+        {
+            String dummy_host_uuid = "dummy_host_uuid";
+
+            InternalMethodCaller endpt = new InternalMethodCaller(
+                new RalphGlobals(),
+                dummy_host_uuid,
+                new SingleSideConnection());
+
+            // testing numbers
+            double original_number = endpt.get_number().doubleValue();
+
+            for (int i = 1; i <= 20; ++i)
+            {
+                endpt.increment_number();
+                double new_number = endpt.get_number().doubleValue();
+                if (new_number != (original_number + i))
+                    return false;
+            }
+            
+            return true;
+        }
+        catch(Exception _ex)
+        {
+            _ex.printStackTrace();
+            return false;
+        }
+    }
+}
