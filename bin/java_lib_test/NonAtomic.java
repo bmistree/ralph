558fe3211707e0b0810bc1e816210954476fc089
behram mistree <bmistree@stanford.edu>
2013-12-06 12:28:40 -0800

    Started adding very basic non-atomic event tests (eg., reading a single value).

---
+++ bin/java_lib_test/NonAtomic.java
@@ -0,0 +1,74 @@
+package java_lib_test;
+
+import ralph.VariableStack;
+import ralph.LockedVariables.LockedNumberVariable;
+import ralph.LockedObject;
+import ralph.Endpoint;
+import ralph.ActiveEvent;
+import ralph.RootEventParent;
+import RalphCallResults.RootCallResult.ResultType;
+
+/**
+   Creates an active event that reads and writes to tvar.  checks that
+   can have multiple readers of tvar at same time.
+ */
+
+public class NonAtomic
+{
+    protected static String test_name = "NonAtomic";
+
+    public static void main(String [] args)
+    {
+        if (NonAtomic.run_test())
+            TestClassUtil.print_success(test_name);
+        else
+            TestClassUtil.print_failure(test_name);
+    }
+
+    public static boolean run_test()
+    {
+        Endpoint endpt = TestClassUtil.create_default_single_endpoint();
+        LockedNumberVariable num_tvar =
+            (LockedNumberVariable) endpt.global_var_stack.get_var_if_exists(
+                TestClassUtil.DefaultEndpoint.NUM_TVAR_NAME);
+
+        // Tests concurrent read of tvar.
+        if (! NonAtomic.test_non_atomic_read(endpt,num_tvar))
+            return false;
+
+        return true;
+    }
+
+    public static boolean test_non_atomic_read(
+        Endpoint endpt, LockedNumberVariable num_tvar)
+    {
+        try
+        {
+            ActiveEvent reader =
+                endpt._act_event_map.create_root_event(false);
+
+            if (! num_tvar.get_val(reader).equals(
+                    TestClassUtil.DefaultEndpoint.NUM_TVAR_INIT_VAL))
+            {
+                return false;
+            }
+            reader.begin_first_phase_commit();
+            RootEventParent reader_event_parent =
+                (RootEventParent)reader.event_parent;
+            ResultType reader_commit_resp =
+                reader_event_parent.event_complete_queue.take();
+
+            if (reader_commit_resp != ResultType.COMPLETE)
+                return false;
+            
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    
+}
+
