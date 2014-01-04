3711682371ee92ddd5221f8bc9a7c08d683881c7
bmistree <bmistree@stanford.edu>
2014-01-03 18:52:14 -0800

    Added test for for loop on maps.

---
+++ bin/emit_test/test_src/ForMap.java
@@ -0,0 +1,64 @@
+package emit_test_harnesses;
+
+import emit_test_package.ForMapTest.MapEndpoint;
+import RalphConnObj.SingleSideConnection;
+import ralph.RalphGlobals;
+
+public class ForMap
+{
+    public static void main(String[] args)
+    {
+        if (ForMap.run_test())
+            System.out.println("\nSUCCESS in ForMap\n");
+        else
+            System.out.println("\nFAILURE in ForMap\n");
+    }
+    
+    public static boolean run_test()
+    {
+        try
+        {
+            String dummy_host_uuid = "dummy_host_uuid";
+            
+            MapEndpoint endpt = new MapEndpoint(
+                new RalphGlobals(),
+                dummy_host_uuid,
+                new SingleSideConnection());
+
+            /** Check on non-atomic map */
+            // insert number in map
+            double sum_of_values_in_map = 0;
+            for (int i = 5; i < 20; ++i)
+            {
+                Double to_insert = new Double((double)i);
+                Double to_insert_index = new Double((double)i+30);
+                sum_of_values_in_map += to_insert;
+                endpt.insert_number(to_insert_index,to_insert);
+            }
+            // calculate sum
+            if (endpt.sum_map_numbers().doubleValue() != sum_of_values_in_map)
+                return false;
+
+            /** Check on atomic map */
+            // insert number in map
+            sum_of_values_in_map = 0;
+            for (int i = 5; i < 20; ++i)
+            {
+                Double to_insert = new Double((double)i);
+                Double to_insert_index = new Double((double)i+30);
+                sum_of_values_in_map += to_insert;
+                endpt.atomic_insert_number(to_insert_index,to_insert);
+            }
+            // calculate sum
+            if (endpt.atomic_sum_map_numbers().doubleValue() != sum_of_values_in_map)
+                return false;
+
+            return true;
+        }
+        catch(Exception _ex)
+        {
+            _ex.printStackTrace();
+            return false;
+        }
+    }
+}
