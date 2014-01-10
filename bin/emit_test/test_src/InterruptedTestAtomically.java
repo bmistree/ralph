473f8c55b95518dd394b4deb0849e5cb2e08b639
behram mistree <bmistree@gmail.com>
2014-01-10 14:33:12 -0800

    Expanded interrupted atomically tests for nested atomically-s. Checking that will retry event even if backed out from multiple levels of atomically.

--- bin/emit_test/test_src/InterruptedTestAtomically.java
+++ bin/emit_test/test_src/InterruptedTestAtomically.java
@@ -16,9 +16,9 @@ public class InterruptedTestAtomically
     final static int NUM_TIMES_TO_TRY_TO_FORCE = 50;
     final static int NUM_TO_INITIALLY_APPEND = 50;
     final static int NUM_MS_TO_WAIT_TO_COMPLETE = 400* 1000;
-    static AtomicBoolean was_interrupted = new AtomicBoolean(false);
-    static AtomicBoolean had_exception = new AtomicBoolean(false);
-    static AtomicInteger num_completed = new AtomicInteger(0);
+    static AtomicBoolean was_interrupted; 
+    static AtomicBoolean had_exception;
+    static AtomicInteger num_completed;
 
     public enum ReturnCode
     {
@@ -40,6 +40,7 @@ public class InterruptedTestAtomically
 
     public static ReturnCode run_test()
     {
+        ReturnCode status = ReturnCode.SUCCESS;
         try
         {
             String dummy_host_uuid = "dummy_host_uuid";
@@ -49,65 +50,97 @@ public class InterruptedTestAtomically
                 dummy_host_uuid,
                 new SingleSideConnection());
 
-            for (int i = 0; i < NUM_TO_INITIALLY_APPEND; ++i)
+            // failure report takes precedence over unknown report
+            // takes precedence over success report
+            ReturnCode unnested_return_code = long_event(endpt,false);
+            if (unnested_return_code != ReturnCode.SUCCESS)
             {
-                double diff = endpt.long_event_and_append().doubleValue();
-                if (diff != 0.)
-                    return ReturnCode.FAILURE;
-            }
-            
-            if (! endpt.list_size().equals((double)NUM_TO_INITIALLY_APPEND))
-                return ReturnCode.FAILURE;
-
-            
-            ExecutorService executor = Executors.newFixedThreadPool(
-                NUM_THREADS,
-                new ThreadFactory()
-                {
-                    // each thread created is a daemon
-                    public Thread newThread(Runnable r)
-                    {
-                        Thread t=new Thread(r);
-                        t.setDaemon(true);
-                        return t;
-                    }
-                });
-
-            TryToInterrupt tti = new TryToInterrupt(endpt);
-            for (int i = 0; i < NUM_TIMES_TO_TRY_TO_FORCE; ++i)
-                executor.execute(tti);
-
-            int i = 0;
-            int ms_to_sleep = 500;
-            while (num_completed.get() != NUM_TIMES_TO_TRY_TO_FORCE)
-            {
-                i++;
-                // System.out.println(
-                //     Integer.toString(num_completed.get()) + " of " +
-                //     Integer.toString(NUM_TIMES_TO_TRY_TO_FORCE));
-                Thread.sleep(ms_to_sleep);
-                int time_slept_ms = ms_to_sleep * i;
-                if (time_slept_ms > NUM_MS_TO_WAIT_TO_COMPLETE)
-                    break;
+                if (status != ReturnCode.FAILURE)
+                    status = unnested_return_code;
             }
 
-            executor.shutdownNow();
-
-            if (had_exception.get())
-                return ReturnCode.FAILURE;
-            
-            if (endpt.list_size() != ((double) (NUM_TO_INITIALLY_APPEND + NUM_TIMES_TO_TRY_TO_FORCE)))
-                return ReturnCode.FAILURE;
-            
-            if (! was_interrupted.get())
-                return ReturnCode.UNKNOWN;
-            
+            ReturnCode nested_return_code = long_event(endpt,true);
+            if (nested_return_code != ReturnCode.SUCCESS)
+            {
+                if (status != ReturnCode.FAILURE)
+                    status = nested_return_code;
+            }
         }
         catch (Exception _ex)
         {
             _ex.printStackTrace();
             return ReturnCode.FAILURE;
         }
+
+        return status;
+        
+    }
+
+    public static ReturnCode long_event(
+        InterruptedAtomic endpt,boolean nested) throws Exception
+    {
+        was_interrupted = new AtomicBoolean(false);
+        had_exception = new AtomicBoolean(false);
+        num_completed = new AtomicInteger(0);
+
+        endpt.clear_list();
+        for (int i = 0; i < NUM_TO_INITIALLY_APPEND; ++i)
+        {
+            double diff = 0;
+            if (nested)
+                diff = endpt.nested_long_event_and_append().doubleValue();
+            else
+                diff = endpt.long_event_and_append().doubleValue();
+            
+            if (diff != 0.)
+                return ReturnCode.FAILURE;
+        }
+
+        if (! endpt.list_size().equals((double)NUM_TO_INITIALLY_APPEND))
+            return ReturnCode.FAILURE;
+
+        ExecutorService executor = Executors.newFixedThreadPool(
+            NUM_THREADS,
+            new ThreadFactory()
+            {
+                // each thread created is a daemon
+                public Thread newThread(Runnable r)
+                {
+                    Thread t=new Thread(r);
+                    t.setDaemon(true);
+                    return t;
+                }
+            });
+
+        TryToInterrupt tti = new TryToInterrupt(endpt,nested);
+        for (int i = 0; i < NUM_TIMES_TO_TRY_TO_FORCE; ++i)
+            executor.execute(tti);
+
+        int i = 0;
+        int ms_to_sleep = 500;
+        while (num_completed.get() != NUM_TIMES_TO_TRY_TO_FORCE)
+        {
+            i++;
+            // System.out.println(
+            //     Integer.toString(num_completed.get()) + " of " +
+            //     Integer.toString(NUM_TIMES_TO_TRY_TO_FORCE));
+            Thread.sleep(ms_to_sleep);
+            int time_slept_ms = ms_to_sleep * i;
+            if (time_slept_ms > NUM_MS_TO_WAIT_TO_COMPLETE)
+                break;
+        }
+
+        executor.shutdownNow();
+
+        if (had_exception.get())
+            return ReturnCode.FAILURE;
+
+        if (endpt.list_size() != ((double) (NUM_TO_INITIALLY_APPEND + NUM_TIMES_TO_TRY_TO_FORCE)))
+            return ReturnCode.FAILURE;
+
+        if (! was_interrupted.get())
+            return ReturnCode.UNKNOWN;
+        
         return ReturnCode.SUCCESS;
     }
 
@@ -115,16 +148,24 @@ public class InterruptedTestAtomically
     public static class TryToInterrupt implements Runnable
     {
         private InterruptedAtomic endpt;
-        public TryToInterrupt(InterruptedAtomic _endpt)
+        private boolean nested;
+        public TryToInterrupt(InterruptedAtomic _endpt,boolean _nested)
         {
             endpt = _endpt;
+            nested = _nested;
         }
 
         public void run()
         {
             try {
-                double diff = endpt.long_event_and_append().doubleValue();
+                double diff = 0;
+                if (nested)
+                    diff = endpt.nested_long_event_and_append().doubleValue();
+                else
+                    diff = endpt.long_event_and_append().doubleValue();
+                
                 num_completed.getAndIncrement();
+                
                 if (diff != 0.)
                     was_interrupted.set(true);
             } catch (Exception ex)
