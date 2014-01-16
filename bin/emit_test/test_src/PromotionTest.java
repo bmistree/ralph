418f572f0db05231d2feb9a730622b6f0fe5bf01
behram mistree <bmistree@gmail.com>
2014-01-15 21:03:18 -0800

    Cleaned up test to ensure that boosted scheduling algorithm runs correctly.  Added more comments and actually checked ordering of results instead of just printing them to screen and making runner manually verify.

--- bin/emit_test/test_src/PromotionTest.java
+++ bin/emit_test/test_src/PromotionTest.java
@@ -9,6 +9,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.lang.Math;
 
 /**
    Reproduced from top of promotion.rph file.
@@ -18,23 +19,25 @@ import java.util.concurrent.atomic.AtomicBoolean;
    bunch of call_from_external calls on A (with arg true).  Before these
    complete, issue a bunch of call_from_external calls on B (with arg false).
 
-   This means that we'll increment my_number a bunch on A and decrement
-   my_number a bunch on B.  
+   This means that we'll cause a bunch on both A and B.
 
    Put results of each call into a threadsafe array.  After all events are run,
    look through the array.  If promotion is set up correctly, then should get a
-   positive number in array, then a negative number in array, then a positive
-   number, etc.  (All positives should be strictly increasing; all negatives
-   should be strictly decreasing.)
- */
+   1 in array, then a -1 in array, then 1, etc.
 
+   Note: there could occasionally be some out-of-orderness, as we can't
+   guarantee that the result of one transaction gets put into the threadsafe
+   queue before other transactions that completed after it.  But the timeouts
+   that we've selected seem as though they may reasonably ensure ordering.
+
+*/
 public class PromotionTest
 {
     // put results into this queue
     final static ConcurrentLinkedQueue<Double> tsafe_queue =
         new ConcurrentLinkedQueue<Double>();
     final static int NUM_THREADS_EACH_SIDE = 10;
-    final static int NUM_EXTERNAL_CALLS = 100;
+    final static int NUM_EXTERNAL_CALLS = 40;
     final static AtomicBoolean had_exception = new AtomicBoolean(false);
     
     public static void main(String[] args)
@@ -80,8 +83,7 @@ public class PromotionTest
             EndpointTask task_b = new EndpointTask(side_b,false);
 
             
-            // put a bunch of tasks to increment a's number into
-            // system.
+            // put a bunch of tasks rooted at A into system.
             for (int i = 0; i < NUM_EXTERNAL_CALLS; ++i)
                 executor_a.execute(task_a);
 
@@ -89,8 +91,7 @@ public class PromotionTest
             // started.
             
             Thread.sleep(50);
-            // put a bunch of tasks to increment b's number into
-            // system.
+            // put a bunch of tasks rooted at B into system.
             for (int i = 0; i < NUM_EXTERNAL_CALLS; ++i)
                 executor_b.execute(task_b);
             
@@ -101,13 +102,19 @@ public class PromotionTest
             while (!(executor_a.isTerminated() && executor_b.isTerminated()))
                 Thread.sleep(50);
             
-            // For now, just scan through queue to ensure ordering
+            // Scan through array: events should alternate.  This means that
+            // should see 1, then -1, then 1, then -1, etc.: a partial sum of
+            // the array should never yield a result where abs of the sum > 1.
+            int running_sum = 0;
             for (Double d : tsafe_queue)
-                System.out.println(d);
+            {
+                running_sum += d.intValue();
+                if (Math.abs(running_sum) > 1)
+                    return false;
+            }
 
             if (had_exception.get())
                 return false;
-            
 
         }
         catch(Exception _ex)
@@ -123,20 +130,20 @@ public class PromotionTest
     public static class EndpointTask implements Runnable
     {
         private PromoterEndpoint endpt_to_run_on = null;
-        private boolean is_increment = false;
+        private boolean is_positive = false;
 
         public EndpointTask(
-            PromoterEndpoint _endpt_to_run_on,boolean _is_increment)
+            PromoterEndpoint _endpt_to_run_on,boolean _is_positive)
         {
-            is_increment = _is_increment;
+            is_positive = _is_positive;
             endpt_to_run_on = _endpt_to_run_on;
-
         }
 
         public void run()
         {
             try {
-                Double result = endpt_to_run_on.call_from_external(is_increment);
+                Double result =
+                    endpt_to_run_on.call_from_external(is_positive);
                 tsafe_queue.add(result);
             } catch(Exception ex)
             {
