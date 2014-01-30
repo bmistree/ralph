5dd869ff70f771ebda82a209c237cd13787f9108
behram mistree <bmistree@gmail.com>
2014-01-30 00:20:19 -0800

    Added names that each endpoint prints when it returns to simplify debugging.

--- bin/emit_test/test_src/LinkedTests.java
+++ bin/emit_test/test_src/LinkedTests.java
@@ -47,6 +47,7 @@ public class LinkedTests
             // a
             LinkedInstanceEndpoint a = new LinkedInstanceEndpoint(
                 ab_globals,"", new SingleSideConnection());
+            a.set_name("A");
 
             // connect b and c
             SameHostConnection bc_conn = new SameHostConnection();
@@ -54,22 +55,28 @@ public class LinkedTests
                 ab_globals,"",bc_conn);
             LinkedConnectionEndpoint c = new LinkedConnectionEndpoint(
                 cde_globals,"",bc_conn);
-
+            b.set_name("B");
+            c.set_name("C");
+            
             // d
             LinkedInstanceEndpoint d = new LinkedInstanceEndpoint(
                 cde_globals,"", new SingleSideConnection());
-
+            d.set_name("D");
+            
             // connect e and f
             SameHostConnection ef_conn = new SameHostConnection();
             LinkedConnectionEndpoint e = new LinkedConnectionEndpoint(
                 cde_globals,"",ef_conn);
             LinkedConnectionEndpoint f = new LinkedConnectionEndpoint(
                 fg_globals,"",ef_conn);
-            
+            e.set_name("E");
+            f.set_name("F");
+
             // create g
             LinkedInstanceEndpoint g = new LinkedInstanceEndpoint(
                 fg_globals,"", new SingleSideConnection());
-
+            g.set_name("G");
+            
             
             // add directional connections
 
