d2e048c9d9338c1515797a418e63fc908023ce5f
behram mistree <bmistree@gmail.com>
2014-02-11 15:35:12 -0800

    Refactoring speculation test to reuse same methods for both mixed and unmixed speculation pipelines.

--- bin/emit_test/test_src/BasicSpeculationTest.java
+++ bin/emit_test/test_src/BasicSpeculationTest.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.List;
 import java.util.ArrayList;
-
+import java.util.concurrent.atomic.AtomicLong;
 
 public class BasicSpeculationTest
 {
@@ -21,30 +21,75 @@ public class BasicSpeculationTest
 
     public static boolean run_test()
     {
-        return speculation_uninterrupted() &&
-            speculation_interrupted();
+        return all_unmixed_speculation_uninterrupted() &&
+            all_unmixed_speculation_interrupted() &&
+            all_mixed_speculation_tests();
     }
 
+    public static boolean all_mixed_speculation_tests()
+    {
+        return true;
+    }
+    
     /**
        When speculating, later must backout the speculation.
      */
-    public static boolean speculation_interrupted()
+    public static boolean all_unmixed_speculation_interrupted()
     {
-        
-        return speculation_test(true,2) &&
-            speculation_test(true,4) &&
-            speculation_test(true,8);
+        return unmixed_speculation_test(true,2) &&
+            unmixed_speculation_test(true,4) &&
+            unmixed_speculation_test(true,8);
     }
 
-    public static boolean speculation_uninterrupted()
+    public static boolean all_unmixed_speculation_uninterrupted()
     {
-        return speculation_test(false,2) &&
-            speculation_test(false,4) &&
-            speculation_test(false,8);
+        return unmixed_speculation_test(false,2) &&
+            unmixed_speculation_test(false,4) &&
+            unmixed_speculation_test(false,8);
     }
 
-    public static boolean speculation_test(
+
+    public static boolean unmixed_speculation_test(
         boolean interrupt_speculation, int num_events_in_pipeline)
+    {
+        List<Boolean> interrupt_speculation_pattern =
+            new ArrayList<Boolean>();
+        for (int i = 0; i < num_events_in_pipeline; ++i)
+            interrupt_speculation_pattern.add(interrupt_speculation);
+
+        AtomicLong speculation_off_ms_runtime = new AtomicLong();
+        AtomicLong speculation_on_ms_runtime = new AtomicLong();
+
+        boolean worked = mixed_speculation_test(
+            interrupt_speculation_pattern,speculation_off_ms_runtime,
+            speculation_on_ms_runtime);
+
+        if (! worked)
+            return false;
+
+        if (! interrupt_speculation)
+        {
+            long time_diff =
+                speculation_off_ms_runtime.get() - speculation_on_ms_runtime.get();
+            // each event sleeps for 500 ms we start events staggered by 10 ms.
+            // (pad to 11 to handle minor overheads)
+            if (time_diff < (500 - 11*num_events_in_pipeline))
+                return false;
+        }
+        
+        return true;
+    }
+
+
+    /**
+       @param interrupted_test_pattern --- Run a separate thread for each
+       element in this list.  If the element is true, then run a pipeline
+       interrupted test.  If it is false, then run just a pipeline test.
+     */
+    public static boolean mixed_speculation_test(
+        List<Boolean> interrupt_speculation_pattern,
+        AtomicLong speculation_off_ms_runtime,
+        AtomicLong speculation_on_ms_runtime)
     {
         try
         {
@@ -58,29 +103,21 @@ public class BasicSpeculationTest
             // just check pipeline without speculation
             double amt_to_inc_by = 35.0;
 
-            List<Boolean> interrupt_speculation_pattern =
-                new ArrayList<Boolean>();
-            for (int i = 0; i < num_events_in_pipeline; ++i)
-                interrupt_speculation_pattern.add(interrupt_speculation);
             
             long speculation_off_ms = pipelined_events_time_ms(
                 endpt,false,amt_to_inc_by,interrupt_speculation_pattern);
             long speculation_on_ms = pipelined_events_time_ms(
                 endpt,true,amt_to_inc_by,interrupt_speculation_pattern);
 
-            if (! interrupt_speculation)
-            {
-                // if speculation was not interrupted, then should
-                // have been able to pipeline two calls.
-                if ((speculation_off_ms - speculation_on_ms) < 400)
-                    return false;
-            }
-
+            if (speculation_off_ms_runtime != null)
+                speculation_off_ms_runtime.set(speculation_off_ms);
+            if (speculation_on_ms_runtime != null)
+                speculation_on_ms_runtime.set(speculation_on_ms);
             
             double new_internal_number = endpt.get_number().doubleValue();
             double expected_internal_number =
                 original_internal_number +
-                2*num_events_in_pipeline*amt_to_inc_by;
+                2*interrupt_speculation_pattern.size()*amt_to_inc_by;
             
             if (new_internal_number != expected_internal_number)
                 return false;
