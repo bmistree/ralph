e58806c8a2979f79b53786513e1f8e1c771d41ab
behram mistree <bmistree@gmail.com>
2014-02-11 10:52:53 -0800

    Updated speculative tests to force backout of a speculating event. Test currently fails.

--- bin/emit_test/test_src/BasicSpeculationTest.java
+++ bin/emit_test/test_src/BasicSpeculationTest.java
@@ -16,6 +16,26 @@ public class BasicSpeculationTest
 
     public static boolean run_test()
     {
+        return speculation_uninterrupted() &&
+            speculation_interrupted();
+    }
+
+    /**
+       When speculating, later must backout the speculation.
+     */
+    public static boolean speculation_interrupted()
+    {
+        return speculation_test(true);
+    }
+
+    public static boolean speculation_uninterrupted()
+    {
+        return speculation_test(false);
+    }
+    
+
+    public static boolean speculation_test(boolean interrupt_speculation)
+    {
         try
         {
             SpeculativeEndpoint endpt = new SpeculativeEndpoint(
@@ -28,12 +48,17 @@ public class BasicSpeculationTest
             // just check pipeline without speculation
             double amt_to_inc_by = 35.0;
             long speculation_off_ms = back_to_back_events_time_ms(
-                endpt,false,amt_to_inc_by);
+                endpt,false,amt_to_inc_by,interrupt_speculation);
             long speculation_on_ms = back_to_back_events_time_ms(
-                endpt,true,amt_to_inc_by);
-            
-            if ((speculation_off_ms - speculation_on_ms) < 400)
-                return false;
+                endpt,true,amt_to_inc_by,interrupt_speculation);
+
+            if (! interrupt_speculation)
+            {
+                // if speculation was not interrupted, then should
+                // have been able to pipeline two calls.
+                if ((speculation_off_ms - speculation_on_ms) < 400)
+                    return false;
+            }
 
             
             double new_internal_number = endpt.get_number().doubleValue();
@@ -57,20 +82,25 @@ public class BasicSpeculationTest
         private Double amt_to_inc_by = null;
         private boolean speculate;
         public boolean had_exception = false;
+        private boolean interrupt_speculation = false;
         
         public SingleOp(
             SpeculativeEndpoint endpt, Double amt_to_inc_by,
-            boolean speculate)
+            boolean speculate, boolean interrupt_speculation)
         {
             this.endpt = endpt;
             this.amt_to_inc_by = amt_to_inc_by;
             this.speculate = speculate;
+            this.interrupt_speculation = interrupt_speculation;
         }
         public void run()
         {
             try
             {
-                endpt.pipeline(amt_to_inc_by,speculate);
+                if (interrupt_speculation)
+                    endpt.pipeline_interrupted(amt_to_inc_by,speculate);
+                else
+                    endpt.pipeline(amt_to_inc_by,speculate);
             }
             catch (Exception ex)
             {
@@ -82,14 +112,16 @@ public class BasicSpeculationTest
     
     private static long back_to_back_events_time_ms(
         SpeculativeEndpoint endpt,boolean speculate,
-        double amt_to_inc_by) throws Exception
+        double amt_to_inc_by,boolean interrupt_speculation) throws Exception
     {
         long start = System.currentTimeMillis();
-        SingleOp t = new SingleOp(endpt,amt_to_inc_by,speculate);
+        SingleOp t =
+            new SingleOp(endpt,amt_to_inc_by,speculate,interrupt_speculation);
         t.start();
         Thread.sleep(10);
 
-        SingleOp t2 = new SingleOp(endpt,amt_to_inc_by,speculate);
+        SingleOp t2 =
+            new SingleOp(endpt,amt_to_inc_by,speculate,interrupt_speculation);
         t2.start();
 
         t.join();
