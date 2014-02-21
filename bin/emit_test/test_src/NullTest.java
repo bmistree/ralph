e0a051b117aca8fd2a74399ef50fcb76b7fd32bf
behram mistree <bmistree@gmail.com>
2014-02-21 14:52:10 -0800

    Adding a test to ensure handling null correctly.  It currently fails.

---
+++ bin/emit_test/test_src/NullTest.java
@@ -0,0 +1,34 @@
+package emit_test_harnesses;
+
+import emit_test_package.Null.NullService;
+import RalphConnObj.SingleSideConnection;
+import ralph.RalphGlobals;
+
+public class NullTest
+{
+    public static void main(String[] args)
+    {
+        if (NullTest.run_test())
+            System.out.println("\nSUCCESS in NullTest\n");
+        else
+            System.out.println("\nFAILURE in NullTest\n");
+    }
+
+    public static boolean run_test()
+    {
+        try
+        {
+            NullService service = new NullService(
+                new RalphGlobals(),
+                new SingleSideConnection());
+
+            if (service.get_number() != null)
+                return false;
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+}
