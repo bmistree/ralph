8c138a9cc7fe2d51a6e24b9518ac33ea5fb1d748
behram mistree <bmistree@gmail.com>
2014-02-21 16:24:29 -0800

    Updated null test to test for nulls in maps.

--- bin/emit_test/test_src/NullTest.java
+++ bin/emit_test/test_src/NullTest.java
@@ -22,6 +22,7 @@ public class NullTest
                 new RalphGlobals(),
                 new SingleSideConnection());
 
+            /** Test value types */
             if (service.get_number() != null)
                 return false;
 
@@ -31,7 +32,6 @@ public class NullTest
             if (service.get_tf() != null)
                 return false;
 
-
             Double [] doubles_to_test = {3.3,45.,null,null,30.1,null};
             for (Double d : doubles_to_test)
             {
@@ -39,7 +39,7 @@ public class NullTest
                     return false;
             }
 
-            String [] strings_to_test = {"hi","hi",null,"hie",null};
+            String [] strings_to_test = {"hi","hi",null,"hie",null,"mm"};
             for (String s : strings_to_test)
             {
                 if (! check_set_text(service,s))
@@ -52,6 +52,17 @@ public class NullTest
                 if (! check_set_tf(service,b))
                     return false;
             }
+
+
+            /** Test container types */
+            if (service.get_map() != null)
+                return false;
+            service.reset_map();
+            Double [] index_list = {3.3, 4.5,null,3.3,null,30.1, 3.3};
+            Double [] value_list = {3.3,null,null,3.3,null,30.1,null};
+            if (! check_set_map_indices(service,index_list,value_list))
+                return false;
+            
         }
         catch (Exception ex)
         {
@@ -95,5 +106,37 @@ public class NullTest
             return to_set_to == service.get_tf();
 
         return service.get_tf().equals(to_set_to);
-    }    
+    }
+
+    /**
+       Note: assumes that index_list and value list are same size.
+     */
+    private static boolean check_set_map_indices(
+        NullService service, Double [] index_list, Double [] value_list)
+        throws Exception
+    {
+        if (index_list.length != value_list.length)
+        {
+            System.err.println(
+                "Tester err index and value list should be same size");
+            return false;
+        }
+
+        int num_to_test = index_list.length;
+
+        for (int i = 0; i < num_to_test; ++i)
+        {
+            Double index = index_list[i];
+            Double value = value_list[i];
+            service.set_element(index,value);
+
+            Double actual_value = service.get_element(index);
+            if (value == null)
+                return value == actual_value;
+
+            if (! value.equals(actual_value))
+                return false;
+        }
+        return true;
+    }
 }
