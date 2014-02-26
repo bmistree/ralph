4e8d27f27278cedcdbf53372f7a76c8f7edc8ed2
behram mistree <bmistree@gmail.com>
2014-02-25 18:29:43 -0800

    Added scripts for running preprocessor.

---
+++ bin/preprocessor_parse_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sys
+import os
+
+base_path = os.path.join(
+    os.path.realpath(os.path.dirname(__file__)),'..')
+sys.path.append(base_path)
+from ralph.parse.ast_builder import construct_parser
+
+from ralph.preprocessor_parse.preprocessor_ast_builder import construct_parser
+
+
+def preprocessor_parse(filename,suppress_warnings=True):
+    file_fd = open(filename,'r')
+    prog_text = file_fd.read()
+    file_fd.close()
+
+    parser = construct_parser(suppress_warnings)
+    return parser.parse(prog_text)
+
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    preprocessor_parse(filename,False)
