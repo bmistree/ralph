7121b821ce70ee689d8972bb83ba9cede39b0c88
behram mistree <bmistree@gmail.com>
2014-03-27 18:54:32 -0700

    Added a more-involved example of concatenating text and converting to text.  This example causes everything to break.  Must fix.

--- bin/emit_test/emit_test_harnesses/TextManipulation.java
+++ bin/emit_test/emit_test_harnesses/TextManipulation.java
@@ -33,6 +33,21 @@ public class TextManipulation
             result = endpt.convert(some_double);
             if (! result.equals(some_double.toString()))
                 return false;
+
+
+            Double some_double2 = new Double (4);
+            Double some_double3 = new Double (5);
+
+            result = endpt.many_add_and_convert(
+                some_double,lhs,some_double2,some_double3,
+                rhs);
+
+            String expected =
+                some_double.toString() + lhs + some_double2.toString() +
+                some_double3.toString() + rhs;
+            
+            if (! expected.equals(result))
+                return false;
         }
         catch (Exception _ex)
         {
