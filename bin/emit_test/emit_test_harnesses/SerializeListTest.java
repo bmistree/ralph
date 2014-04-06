2ced6d7ce95c25b00d9f3e45a25c779e81b9fd8d
bmistree <bmistree@gmail.com>
2014-04-06 12:11:23 -0700

    Updated list serialiation test to serialize tvar list.  It fails.  Must fix.

--- bin/emit_test/emit_test_harnesses/SerializeListTest.java
+++ bin/emit_test/emit_test_harnesses/SerializeListTest.java
@@ -24,20 +24,13 @@ public class SerializeListTest
             ListSerializer side_b = new ListSerializer(
                 new RalphGlobals(),conn_obj);
 
-            Double[] max_list_vals_to_sum =
-                new Double [] {1.0, 2.0, 8.0, 20.0};
-
-            for (int i = 0; i < max_list_vals_to_sum.length; ++i)
-            {
-                Double max_num = max_list_vals_to_sum[i];
-                // the sum of integers from [0 to max_num) is
-                // (max_num)(max_num-1)/2
-                Double expected_value = max_num*(max_num -1)/2.0;
-                            
-                Double result = side_a.sum_numbers(max_num);
-                if (! result.equals(expected_value))
-                    return false;
-            }
+            // tests atomic number list serialization
+            if (! num_list_sum_test(true,side_a))
+                return false;
+            // tests non atomic number list serialization
+            if (! num_list_sum_test(false,side_a))
+                return false;
+            
             return true;
         }
         catch(Exception _ex)
@@ -46,4 +39,31 @@ public class SerializeListTest
             return false;
         }
     }
+
+
+    public static boolean num_list_sum_test(
+        boolean atom,ListSerializer to_call_on)
+        throws Exception
+    {
+        Double[] max_list_vals_to_sum =
+            new Double [] {1.0, 2.0, 8.0, 20.0};
+
+        for (int i = 0; i < max_list_vals_to_sum.length; ++i)
+        {
+            Double max_num = max_list_vals_to_sum[i];
+            // the sum of integers from [0 to max_num) is
+            // (max_num)(max_num-1)/2
+            Double expected_value = max_num*(max_num -1)/2.0;
+
+            Double result = null;
+            if (atom)
+                result = to_call_on.sum_atom_numbers(max_num);
+            else
+                result = to_call_on.sum_numbers(max_num);
+            if (! result.equals(expected_value))
+                return false;
+        }
+        return true;
+    }
+    
 }
