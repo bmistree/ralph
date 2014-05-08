4b68eb3341acaf7c1f6f96d5b467e0d5430fd80e
behram mistree <bmistree@gmail.com>
2014-05-08 15:58:08 -0700

    Added test case for serializing one struct with another struct as a field.

---
+++ bin/emit_test/emit_test_harnesses/SerializeStructWithStruct.java
@@ -0,0 +1,42 @@
+package emit_test_harnesses;
+
+import ralph_emitted.SerializeStructWithStructJava.StructSerializer;
+import RalphConnObj.SameHostConnection;
+import ralph.RalphGlobals;
+import static emit_test_harnesses.SerializeStructHarnessHelper.num_sum_test;
+
+public class SerializeStructWithStruct
+{
+    private final static int TCP_CONNECTION_PORT_A = 20494;
+    private final static int TCP_CONNECTION_PORT_B = 20495;
+    
+    public static void main(String[] args)
+    {
+        if (run_test())
+            System.out.println("\nSUCCESS in SerializeStructWithStruct\n");
+        else
+            System.out.println("\nFAILURE in SerializeStructWithStruct\n");
+    }
+
+    public static boolean run_test()
+    {
+        try
+        {
+            SameHostConnection conn_obj = new SameHostConnection();
+            StructSerializer side_a = new StructSerializer(
+                new RalphGlobals(TCP_CONNECTION_PORT_A),conn_obj);
+            StructSerializer side_b = new StructSerializer(
+                new RalphGlobals(TCP_CONNECTION_PORT_B),conn_obj);
+
+            if (! num_sum_test(side_a))
+                return false;
+            
+            return true;
+        }
+        catch(Exception _ex)
+        {
+            _ex.printStackTrace();
+            return false;
+        }
+    }
+}
