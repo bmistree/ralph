24c9ded7216357f7bb722f944b7c194a7d2b6234
bmistree <bmistree@gmail.com>
2014-08-06 15:33:01 -0700

    Had a bug in order I was processing include dependencies. Example of bug:
    Consider the following files with dependencies.
    
    main.rph includes a.rph and then b.rph
    
    a.rph includes c.rph.
    b.rph includes a.rph.
    
    Order for dependency resolution should then be c.rph, a/b.rph,
    main.rph.  Previous algorithm had a bug.  It would correctly label
    that c.rph should be included before a.rph on first pass.  However,
    when it encountered that b.rph depended on a.rph as well, it would
    increase priority of including a.rph without also then increasing
    priority of including c.rph.  In this way, a.rph might be included
    *before* c.rph and break things.

--- bin/produce_dependencies.py
+++ bin/produce_dependencies.py
@@ -30,7 +30,7 @@ def dependencies_list(input_filename,lib_dir_list):
         
     Returns:
         {list} --- Should parse files from lowest index to highest
-        index.
+        index.  Does not include input_filename.
     '''
     fq_input_filename = os.path.abspath(input_filename)
 
@@ -38,56 +38,76 @@ def dependencies_list(input_filename,lib_dir_list):
         lambda path: os.path.abspath(path),
         lib_dir_list)
 
-    deps_dict = dependencies_dict(fq_input_filename,fq_lib_dir_list)
-    fname_depth_pair_list = sorted(
-        deps_dict.values(), key= lambda val: val.depth,reverse=True)
-    to_return = map(lambda pair: pair.fname,fname_depth_pair_list)
+    to_return = dfs_include(fq_input_filename,fq_lib_dir_list)
+    # Dependency list should not include input filename.
+    to_return = to_return[:-1]
     return to_return
 
-    
-    
-class FNameDepthPair(object):
-    def __init__(self,fname,depth):
-        self.fname = fname
-        self.depth = depth
-    
-    
-def dependencies_dict(fq_input_filename,lib_dir_list, depth = 0,
-                      captured_deps_dict = None):
+def dfs_include(fq_filename,lib_dir_list,already_added_set = None):
     '''
     Args:
+        fq_filename: {string} fully-qualified name of file to get
+        dependencies for.
 
-        fq_input_filename: {String} The fully-qualified name of the
-        file we're looking for include dependencies for.
-    
         lib_dir_list: {list} Each element is a string representing a
         folder to search for dependency libraries in.  Note: the order
         for searching for includes is 1) relative to current folder of
         file looking for includes for, 2) ascending indices of
         lib_dir_list.
 
-        depth: {int} How many steps from the root file we are in the
-        include graph.
-
-        captured_deps_dict: {dict or None} None from first,
-        non-recursive caller.  Each index is a string correspoding to
-        the fully-qualified filename of a ralph library that that
-        fq_input_filename depends on.  Each value is a FNameDepthPair
-        containing the fully-qualified file's name and the priority
-        with which we should import the file.  Ie., we should import
-        files first that have the highest priority.  Priorities are
-        essentially the deepest point distance in the include graph
-        the file was required.
+        already_added_set: {set or None} None if invoked from first,
+        non-recursive caller.  A set of dependencies that we have
+        already added to list of files to download.
         
+    Returns:
+        An ordered list of strings.  Filenames with earlier indices
+        should be downloaded first.
     '''
-    if captured_deps_dict is None:
-        # note: not including myself in this dict
-        captured_deps_dict = {}
-
-    depth = depth + 1
-    fq_input_dir = os.path.dirname(fq_input_filename)
+    # list to return... lower indices mean to download sooner.
+    ordered_download_list = []
     
-    preprocessor_statement_list_node = preprocessor_parse(fq_input_filename)
+    if already_added_set is None:
+        already_added_set = set()
+
+    dependency_set = single_file_deps(fq_filename,lib_dir_list)
+    for dependency in dependency_set:
+        if dependency in already_added_set:
+            continue
+
+        ordered_download_list += dfs_include(
+            dependency,lib_dir_list,already_added_set)
+        already_added_set.add(dependency)
+
+    ordered_download_list.append(fq_filename)
+    already_added_set.add(fq_filename)
+
+    return ordered_download_list
+    
+    
+
+
+def single_file_deps(fq_filename,lib_dir_list):
+    '''
+    Args:
+        fq_filename: {string} fully-qualified name of file to get
+        dependencies for.
+
+        lib_dir_list: {list} Each element is a string representing a
+        folder to search for dependency libraries in.  Note: the order
+        for searching for includes is 1) relative to current folder of
+        file looking for includes for, 2) ascending indices of
+        lib_dir_list.
+        
+    Returns:
+        A set of strings.  Each string is an included file we depend
+        on.
+    '''
+    # ensures uniqueness of dependencies.
+    dep_set = set()
+    
+    fq_input_dir = os.path.dirname(fq_filename)
+    
+    preprocessor_statement_list_node = preprocessor_parse(fq_filename)
     for node in preprocessor_statement_list_node.statement_list:
         # only care about include nodes
         if isinstance(node,IncludeStatementNode):
@@ -105,26 +125,14 @@ def dependencies_dict(fq_input_filename,lib_dir_list, depth = 0,
             for folder in [fq_input_dir] + lib_dir_list:
                 fq_next_to_try_to_include = os.path.join(
                     folder,next_to_try_to_include)
-            
-                if os.path.exists(fq_next_to_try_to_include):
-                    if fq_next_to_try_to_include not in captured_deps_dict:
-                        captured_deps_dict[fq_next_to_try_to_include] = (
-                            FNameDepthPair(fq_next_to_try_to_include,depth))
-                        dependencies_dict(
-                            fq_next_to_try_to_include,lib_dir_list,depth,
-                            captured_deps_dict)
-                    else:
-                        pair = captured_deps_dict[fq_next_to_try_to_include]
-                        if pair.depth < depth:
-                            captured_deps_dict[fq_next_to_try_to_include] = (
-                                FNameDepthPair(fq_next_to_try_to_include,depth))
 
+                if os.path.exists(fq_next_to_try_to_include):
+                    dep_set.add(fq_next_to_try_to_include)
                     found_dependency = True
                     break
 
-            # could not find requested dependency: throw error
             if not found_dependency:
                 raise DependencyException(
                     fq_input_filename,next_to_try_to_include)
-                
-    return captured_deps_dict
+
+    return dep_set
