1d51f95b01a32ad40ff8fb738baef97d947b4155
bmistree <bmistree@gmail.com>
2014-08-26 13:23:43 -0700

    Instead of returning all updates, allow to query for only updates that match fields, or are within particular ranges.

--- ralph/java_lib/RalphVersions/SingleDeviceVersionResponse.java
+++ ralph/java_lib/RalphVersions/SingleDeviceVersionResponse.java
@@ -27,22 +27,76 @@ public class SingleDeviceVersionResponse
         device_id = _device_id;
     }
 
-    public void add_device_update(SingleDeviceUpdate update)
+    synchronized public void add_device_update(SingleDeviceUpdate update)
     {
         update_set.add(update);
     }
 
-    public SingleDeviceUpdateListMessage.Builder produce_msg()
+    /**
+       If any argument is null, means do not use it to filter
+       updates.
+     */
+    synchronized public SingleDeviceUpdateListMessage.Builder
+        produce_msg(Long lower_root_commit_lamport_time,
+                    Long upper_root_commit_lamport_time,
+                    Long lower_local_lamport_time,
+                    Long upper_local_lamport_time,
+                    String root_application_uuid,
+                    String event_name,String event_uuid)
     {
+
         SingleDeviceUpdateListMessage.Builder to_return =
             SingleDeviceUpdateListMessage.newBuilder();
         to_return.setDeviceId(device_id);
 
         for (SingleDeviceUpdate update : update_set)
         {
-            to_return.addUpdateList(
-                update.produce_msg());
+            // filter events that do not match root_commit_lamport_time query
+            if ((lower_root_commit_lamport_time != null) &&
+                (lower_root_commit_lamport_time > update.root_commit_lamport_time))
+            {
+                continue;
+            }
+            if ((upper_root_commit_lamport_time != null) &&
+                (upper_root_commit_lamport_time < update.root_commit_lamport_time))
+            {
+                continue;
+            }
+            // filter events that do not match local_lamport_time query
+            if ((lower_local_lamport_time != null) &&
+                (lower_local_lamport_time > update.local_lamport_time))
+            {
+                continue;
+            }
+            if ((upper_local_lamport_time != null) &&
+                (upper_local_lamport_time < update.local_lamport_time))
+            {
+                continue;
+            }
+
+            // filter events that do not match root_application_uuid
+            if ((root_application_uuid != null) &&
+                (!root_application_uuid.equals(update.root_application_uuid)))
+            {
+                continue;
+            }
+            // filter event name
+            if ((event_name != null) &&
+                (! event_name.equals(update.event_name)))
+            {
+                continue;
+            }
+
+            // filter event_uuid
+            if ((event_uuid != null) &&
+                (! event_uuid.equals(update.event_uuid)))
+            {
+                continue;
+            }
+
+            to_return.addUpdateList(update.produce_msg());
         }
         return to_return;
-    }
+    }    
+    
 }
