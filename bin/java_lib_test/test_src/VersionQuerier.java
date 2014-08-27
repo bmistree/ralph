a286eb05197aa3c162f14820cf22dfd58dd255b4
bmistree <bmistree@gmail.com>
2014-08-26 21:13:56 -0700

    Actually issue version query and parse response in version test case.  Currently, test fails.  Uncertain why.  Debugging now.

--- bin/java_lib_test/test_src/VersionQuerier.java
+++ bin/java_lib_test/test_src/VersionQuerier.java
@@ -3,6 +3,14 @@ package java_lib_test;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
+import java.net.Socket;
+import java.io.IOException;
+
+import ralph_version_protobuffs.VersionMessageProto.VersionMessage;
+import ralph_version_protobuffs.VersionRequestProto.VersionRequestMessage;
+import ralph_version_protobuffs.VersionResponseProto.VersionResponseMessage;
+import ralph_version_protobuffs.SingleDeviceUpdateListProto.SingleDeviceUpdateListMessage;
+import ralph_version_protobuffs.SingleDeviceUpdateProto.SingleDeviceUpdateMessage;
 
 import ralph.LamportClock;
 import ralph.RalphGlobals;
@@ -34,7 +42,7 @@ public class VersionQuerier
     private final static int num_updates_to_test = 1000;
     private final static Random rand = new Random();
 
-    
+    private final static RalphGlobals ralph_globals = new RalphGlobals();
     
     public static void main(String [] args)
     {
@@ -46,8 +54,6 @@ public class VersionQuerier
 
     public static boolean run_test()
     {
-        RalphGlobals ralph_globals = new RalphGlobals();
-
         // create version manager and put a device update list in it.
         VersionManager version_manager = 
             new VersionManager(ralph_globals.clock);
@@ -61,9 +67,13 @@ public class VersionQuerier
         List<Integer> update_values =
             insert_random_updates(single_device_update_list);
 
+
         // actually start running the version server
         start_version_server(version_manager);
-        
+
+        List<Integer> received_update_values = query_for_updates();
+        if (! received_update_values.equals(update_values))
+            return false;
         
         return true;
     }
@@ -75,6 +85,81 @@ public class VersionQuerier
                 version_manager, VERSION_SERVER_IP_ADDRESS,
                 VERSION_SERVER_PORT_NUMBER);
         server_thread.start();
+    }
+
+    /**
+       Returns null if there's an error.
+     */
+    private static List<Integer> query_for_updates()
+    {
+        Socket sock = null;
+
+        try
+        {
+            sock = new Socket(
+                VERSION_SERVER_IP_ADDRESS,VERSION_SERVER_PORT_NUMBER);
+            sock.setTcpNoDelay(true);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+
+        Long query_id = 1L;
+        VersionRequestMessage.Builder request =
+            VersionRequestMessage.newBuilder();
+        request.setQueryId(query_id);
+
+        VersionMessage.Builder vm_builder = VersionMessage.newBuilder();
+        vm_builder.setRequest(request);
+        vm_builder.setTimestamp(ralph_globals.clock.get_int_timestamp());
+        VersionMessage version_message = vm_builder.build();
+        
+        // send query to version manager
+        try
+        {
+            version_message.writeDelimitedTo(sock.getOutputStream());
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+
+        
+        // listen for response
+        VersionMessage vm = null;
+        try
+        {
+            vm = VersionMessage.parseDelimitedFrom(
+                sock.getInputStream());
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+
+        if (! vm.hasResponse())
+            return null;
+        
+        List<Integer> to_return = new ArrayList<Integer>();
+        
+        VersionResponseMessage response_message = vm.getResponse();
+        for (SingleDeviceUpdateListMessage update_list_msg :
+                 response_message.getDeviceListList())
+        {
+            for (SingleDeviceUpdateMessage update_msg :
+                     update_list_msg.getUpdateListList())
+            {
+                Integer returned =
+                    UpdateSerializer.produce_data_from_device_update(
+                        update_msg);
+                to_return.add(returned);
+            }
+        }
+        return to_return;
     }
 
     
@@ -98,7 +183,6 @@ public class VersionQuerier
                     EVENT_NAME,
                     EVENT_UUID,
                     update_value);
-            
             to_return.add(update_value);
         }
         return to_return;
