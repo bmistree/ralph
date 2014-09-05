beb79d222eae9d62695738ceed7dee05c7b50997
bmistree <bmistree@gmail.com>
2014-09-05 16:26:02 -0700

    Added a test to ensure that even after many, many list operations, still get expected results.

---
+++ bin/emit_test/emit_test_harnesses/ManyListOps.java
@@ -0,0 +1,43 @@
+package emit_test_harnesses;
+
+import ralph_emitted.AtomicListJava.TVarListEndpoint;
+import RalphConnObj.SingleSideConnection;
+import ralph.RalphGlobals;
+
+public class ManyListOps
+{
+    public static final int NUMBER_OPS_TO_RUN = 100000;
+    
+    public static void main(String[] args)
+    {
+        if (run_test())
+            System.out.println("\nSUCCESS in ManyListOps\n");
+        else
+            System.out.println("\nFAILURE in ManyListOps\n");
+    }
+    
+    public static boolean run_test()
+    {
+        try
+        {
+            TVarListEndpoint endpt = new TVarListEndpoint(
+                new RalphGlobals(),
+                new SingleSideConnection());
+
+            for (int i = 0; i < NUMBER_OPS_TO_RUN; ++i)
+            {
+                endpt.put_number(-1.,3.);
+                endpt.remove(0.);
+            }
+
+            if (endpt.get_size().intValue() != 0)
+                return false;
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+}
