b73c61615a8dc965a4661bc5cd504804d6017724
bmistree <bmistree@gmail.com>
2014-09-22 23:10:47 -0700

    Added a test case for versioned interfaces.  Compiling it seems to cause an error.  Must fix.

---
+++ bin/emit_test/emit_test_harnesses/VersionedIFace.java
@@ -0,0 +1,36 @@
+package emit_test_harnesses;
+
+import ralph.RalphGlobals;
+import RalphConnObj.SingleSideConnection;
+import ralph_emitted.VersionedIFaceJava.VersionedInterface;
+
+public class VersionedIFace
+{
+    public static void main(String[] args)
+    {
+        if (run_test())
+            System.out.println("\nSUCCESS in VersionedIFace\n");
+        else
+            System.out.println("\nFAILURE in VersionedIFace\n");
+    }
+
+    public static boolean run_test()
+    {
+        try
+        {
+            RalphGlobals.Parameters parameters = new RalphGlobals.Parameters();
+            RalphGlobals ralph_globals = new RalphGlobals(parameters);
+
+            VersionedInterface endpt = new VersionedInterface(
+                ralph_globals,new SingleSideConnection());
+
+            return VersionedSetterGetter.run_test_from_endpt(
+                endpt,ralph_globals,endpt._uuid);
+        }
+        catch(Exception _ex)
+        {
+            _ex.printStackTrace();
+            return false;
+        }
+    }
+}
