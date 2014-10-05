6a7e2bf453afb5b2908d800ea385abab02d026b4
bmistree <bmistree@gmail.com>
2014-10-04 23:40:45 -0700

    Deserializing rpc results.  However, there seems to be a bug: getting no results back from deserialization.

--- ralph/java_lib/ralph/ExecutingEventContext.java
+++ ralph/java_lib/ralph/ExecutingEventContext.java
@@ -312,7 +312,21 @@ public class ExecutingEventContext
             Arguments returned_objs_proto =
                 queue_elem.returned_objs;
 
-            Util.logger_assert("Need to deserialize returned objects");
+            // FIXME: only need a single element, not an entire list
+            // of returned objects.  Using this call instead so that
+            // can repurpose deserialization code
+            List<RalphObject> to_return_list =
+                RPCDeserializationHelper.deserialize_arguments_list(
+                    endpoint.ralph_globals,returned_objs_proto);
+            //// DEBUG
+            if (to_return_list.size() != 1)
+            {
+                Util.logger_assert(
+                    "Should only be able to return single object from rpc.");
+            }
+            //// END DEBUG
+
+            return to_return_list.get(0);
         }
         return null;
     }
