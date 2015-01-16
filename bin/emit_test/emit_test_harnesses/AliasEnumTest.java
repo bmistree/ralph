4f54bc13f4055a9551625daa94ae6b19100d889c
bmistree <bmistree@gmail.com>
2015-01-15 16:52:02 -0800

    Fixed minor test typo.

--- bin/emit_test/emit_test_harnesses/AliasEnumTest.java
+++ bin/emit_test/emit_test_harnesses/AliasEnumTest.java
@@ -19,7 +19,7 @@ public class AliasEnumTest
     {
         try
         {
-            Test endpt = new Test.create_single_sided(new RalphGlobals());
+            Test endpt = Test.create_single_sided(new RalphGlobals());
             return EnumTest.internal_test(endpt);
         }
         catch(Exception _ex)
