74f70ae0b7c74f9c9deb570ebd2c89a2f6809e5a
bmistree <bmistree@gmail.com>
2015-01-27 14:14:34 -0800

    Actually specifying data fields for durability pair class.

--- ralph/java_lib/RalphDurability/DurabilityPair.java
+++ ralph/java_lib/RalphDurability/DurabilityPair.java
@@ -1,5 +1,7 @@
 package RalphDurability;
 
+import ralph_protobuffs.DurabilityPrepareProto.DurabilityPrepare;
+
 public class DurabilityPair
 {
     public enum DurabilityType
@@ -8,5 +10,15 @@ public class DurabilityPair
         COMPLETED,
         // unknown whether message completed.
         OUTSTANDING;
+    }
+
+    final public DurabilityType complete_type;
+    final public DurabilityPrepare prepare_msg;
+
+    public DurabilityPair(
+        DurabilityType complete_type, DurabilityPrepare prepare_msg)
+    {
+        this.complete_type = complete_type;
+        this.prepare_msg = prepare_msg;
     }
 }
