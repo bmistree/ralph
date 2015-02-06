6476ef12c57f3835b3641fa2bf5c2f17c772d772
bmistree <bmistree@gmail.com>
2015-02-05 17:59:56 -0800

    Refactored BasicSetterGetter so that other setter getter tests can use its main static field, just passing in correct settergetter interface to use it.

--- bin/emit_test/emit_test_harnesses/BasicSetterGetter.java
+++ bin/emit_test/emit_test_harnesses/BasicSetterGetter.java
@@ -15,47 +15,53 @@ public class BasicSetterGetter
             System.out.println("\nFAILURE in BasicSetterGetter\n");
     }
 
+    public static boolean isetter_getter_runs(ISetterGetter endpt)
+        throws Exception
+    {
+        // testing numbers
+        double original_internal_number = endpt.get_number().doubleValue();
+        for (int i = 0; i < 20; ++i)
+        {
+            double new_number = original_internal_number + ((double)1);
+            endpt.set_number(new_number);
+            double gotten_number = endpt.get_number().doubleValue();
+            if (gotten_number != new_number)
+                return false;
+        }
+
+        // testing texts
+        String original_internal_text = endpt.get_text();
+        for (int i = 0; i < 20; ++i)
+        {
+            String new_text = original_internal_text + "hello";
+            endpt.set_text(new_text);
+            String gotten_text = endpt.get_text();
+            if (! new_text.equals(gotten_text))
+                return false;
+        }
+
+        // testing tfs
+        boolean original_internal_bool = endpt.get_tf().booleanValue();
+        boolean new_boolean = original_internal_bool;
+        for (int i = 0; i < 20; ++i)
+        {
+            new_boolean = ! new_boolean;
+            endpt.set_tf(new_boolean);
+            boolean gotten_boolean = endpt.get_tf().booleanValue();
+            if (gotten_boolean != new_boolean)
+                return false;
+        }
+
+        return true;
+    }
+    
     public static boolean run_test()
     {
         try
         {
             ISetterGetter endpt =
                 SetterGetter.create_single_sided(new RalphGlobals());
-            
-            // testing numbers
-            double original_internal_number = endpt.get_number().doubleValue();
-            for (int i = 0; i < 20; ++i)
-            {
-                double new_number = original_internal_number + ((double)1);
-                endpt.set_number(new_number);
-                double gotten_number = endpt.get_number().doubleValue();
-                if (gotten_number != new_number)
-                    return false;
-            }
-
-            // testing texts
-            String original_internal_text = endpt.get_text();
-            for (int i = 0; i < 20; ++i)
-            {
-                String new_text = original_internal_text + "hello";
-                endpt.set_text(new_text);
-                String gotten_text = endpt.get_text();
-                if (! new_text.equals(gotten_text))
-                    return false;
-            }
-
-            // testing tfs
-            boolean original_internal_bool = endpt.get_tf().booleanValue();
-            boolean new_boolean = original_internal_bool;
-            for (int i = 0; i < 20; ++i)
-            {
-                new_boolean = ! new_boolean;
-                endpt.set_tf(new_boolean);
-                boolean gotten_boolean = endpt.get_tf().booleanValue();
-                if (gotten_boolean != new_boolean)
-                    return false;
-            }
-
+            isetter_getter_runs(endpt);
             return true;
         }
         catch(Exception _ex)
