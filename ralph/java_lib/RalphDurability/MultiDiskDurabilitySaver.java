b15e08cf06e336b26403ff9af0eca83c5ab98aac
Behram Mistree <bmistree@gmail.com>
2015-03-22 15:48:57 -0700

    Extending durability saver so that it will save in multiple directories, not just multiple files.

--- ralph/java_lib/RalphDurability/MultiDiskDurabilitySaver.java
+++ ralph/java_lib/RalphDurability/MultiDiskDurabilitySaver.java
@@ -2,6 +2,7 @@ package RalphDurability;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.io.File;
 import java.io.IOException;
@@ -11,9 +12,9 @@ import ralph.Util;
 
 public class MultiDiskDurabilitySaver implements IDurabilitySaver
 {
-    private final int num_synced_log_files;
-    private final int num_non_synced_log_files;
-
+    final private int num_synced_savers;
+    final private int num_non_synced_savers;
+    
     // disk durability savers
     private final List<IDurabilitySaver> list_of_synced_savers =
         new ArrayList<IDurabilitySaver>();
@@ -26,41 +27,58 @@ public class MultiDiskDurabilitySaver implements IDurabilitySaver
 
     private final LRUCache<String,Boolean> constructor_obj_index =
         new LRUCache<String,Boolean>();
-    
+
     public MultiDiskDurabilitySaver(
-        String dir_to_save_to, int num_synced_log_files,
-        int num_non_synced_log_files, IDurabilitySaverFactory synced_saver,
+        String dir_to_save_to, int num_synced_log_files_in_dir,
+        int num_non_synced_log_files_in_dir,
+        IDurabilitySaverFactory synced_saver,
         IDurabilitySaverFactory non_synced_saver)
     {
-        this.num_synced_log_files = num_synced_log_files;
-        this.num_non_synced_log_files = num_non_synced_log_files;
-        for (int i = 0; i < num_synced_log_files; ++i)
+        this (
+            new ArrayList<String> (
+                Arrays.asList(
+                    new String[]{dir_to_save_to})),
+            num_synced_log_files_in_dir, num_non_synced_log_files_in_dir,
+            synced_saver, non_synced_saver);
+    }
+    
+    public MultiDiskDurabilitySaver(
+        List<String> dir_to_save_to_list, int num_synced_log_files_per_dir,
+        int num_non_synced_log_files_per_dir, IDurabilitySaverFactory synced_saver,
+        IDurabilitySaverFactory non_synced_saver)
+    {
+        for (String dir_to_save_to : dir_to_save_to_list)
         {
-            String log_filename =
-                dir_to_save_to + File.separator +
-                "durability_log_" + i + ".bin";
+            for (int i = 0; i < num_synced_log_files_per_dir; ++i)
+            {
+                String log_filename =
+                    dir_to_save_to + File.separator +
+                    "durability_log_" + i + ".bin";
 
-            IDurabilitySaver ids = synced_saver.construct(log_filename);
-            list_of_synced_savers.add(ids);
+                IDurabilitySaver ids = synced_saver.construct(log_filename);
+                list_of_synced_savers.add(ids);
+            }
+            for (int i = 0; i <num_non_synced_log_files_per_dir; ++i)
+            {
+                // initialize lazy writers
+                String lazy_filename =
+                    dir_to_save_to + File.separator +
+                    "durability_lazy_log_" + i + ".bin";
+
+                IDurabilitySaver ids =
+                    non_synced_saver.construct(lazy_filename);
+                list_of_non_synced_savers.add(ids);
+            }
         }
 
-        for (int i = 0; i <num_non_synced_log_files; ++i)
-        {
-            // initialize lazy writers
-            String lazy_filename =
-                dir_to_save_to + File.separator +
-                "durability_lazy_log_" + i + ".bin";
-
-            IDurabilitySaver ids =
-                non_synced_saver.construct(lazy_filename);
-            list_of_non_synced_savers.add(ids);
-        }
+        this.num_synced_savers = list_of_synced_savers.size();
+        this.num_non_synced_savers = list_of_non_synced_savers.size();
     }
 
     @Override
     public void prepare_operation(IDurabilityContext dc)
     {
-        int index = synced_index.addAndGet(1) % num_synced_log_files;
+        int index = synced_index.addAndGet(1) % num_synced_savers;
         IDurabilitySaver ids = list_of_synced_savers.get(index);
         ids.prepare_operation(dc);
     }
@@ -85,7 +103,7 @@ public class MultiDiskDurabilitySaver implements IDurabilitySaver
 
         if (should_log_to_disk)
         {
-            int index = synced_index.addAndGet(1) % num_synced_log_files;
+            int index = synced_index.addAndGet(1) % num_synced_savers;
             IDurabilitySaver ids = list_of_synced_savers.get(index);
             ids.ensure_logged_endpt_constructor(endpt_constructor_obj);
         }
@@ -94,7 +112,7 @@ public class MultiDiskDurabilitySaver implements IDurabilitySaver
     @Override
     public void complete_operation(IDurabilityContext dc, boolean succeeded)
     {
-        int index = non_synced_index.addAndGet(1) % num_non_synced_log_files;
+        int index = non_synced_index.addAndGet(1) % num_non_synced_savers;
         IDurabilitySaver ids = list_of_non_synced_savers.get(index);
         ids.complete_operation(dc,succeeded);
     }
