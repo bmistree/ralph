723ddafaa97feeaa0ed64d86788c5ef8b7239625
bmistree <bmistree@gmail.com>
2015-03-30 16:59:55 -0700

    Removed last vestiges of rpc's being handled in active events instead of livemessagesender.

--- ralph/java_lib/ralph/ActiveEvent.java
+++ ralph/java_lib/ralph/ActiveEvent.java
@@ -280,39 +280,6 @@ public abstract class ActiveEvent
     public abstract void handle_backout_exception(BackoutException be)
         throws BackoutException;
 
-    
-    /**
-       @param {String or None} func_name --- When func_name is None,
-       then sending to the other side the message that we finished
-       performing the requested block.  In this case, we do not need
-       to add result_mvar to waiting mvars.
-
-       @param {bool} first_msg --- True if this is the first message
-       in a sequence that we're sending.  Necessary so that we can
-       tell whether or not to force sending sequence local data.
-
-       @param {MVar or null} result_mvar --- None if
-       this was the last message sent in a sequence and we're not
-       waiting on a reply.
-
-       @param {List or null} args --- The positional arguments
-       inserted into the call as an rpc.  Includes whether the
-       argument is a reference or not (ie, we should update the
-       variable's value on the caller).  Note that can be null if we
-       have no args to pass back (or if is a sequence completed call).
-
-       @param {RalphObject} result --- If this is a reply to an rpc
-       and the called method had a return value, then we return it in
-       result.
-       
-       The local endpoint is requesting its partner to call some
-       method on itself.
-    */
-    public abstract boolean issue_partner_sequence_block_call(
-        Endpoint endpoint, LiveMessageSender msg_sender, String func_name,
-        MVar<MessageCallResultObject>result_mvar,
-        boolean first_msg,List<RalphObject>args,RalphObject result);
-
 
     /**
        @param {Endpoint} endpt
