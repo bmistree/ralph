80d92d66b69e2a175ade6e4b1f858b83057849b4
Behram Mistree <behrammistree@forwardnetworks.com>
2015-05-25 12:20:15 -0700

    Updated partner call test to use new install syntax.

--- bin/emit_test/emit_test_harnesses/PartnerCallReturn.java
+++ bin/emit_test/emit_test_harnesses/PartnerCallReturn.java
@@ -1,14 +1,15 @@
 package emit_test_harnesses;
 
 import ralph_emitted.PartnerNumberReturnJava.PassReturnArg;
-import RalphConnObj.SameHostConnection;
 import ralph.RalphGlobals;
+import ralph.Ralph;
+import ralph.InternalServiceFactory;
 
 public class PartnerCallReturn
 {
     private final static int TCP_CONNECTION_PORT_A = 20494;
     private final static int TCP_CONNECTION_PORT_B = 20495;
-    
+
     public static void main(String[] args)
     {
         if (run_test())
@@ -19,19 +20,29 @@ public class PartnerCallReturn
 
     public static boolean run_test()
     {
-        RalphGlobals.Parameters params_a = new RalphGlobals.Parameters();
-        params_a.tcp_port_to_listen_for_connections_on = TCP_CONNECTION_PORT_A;
-        
-        RalphGlobals.Parameters params_b = new RalphGlobals.Parameters();
-        params_b.tcp_port_to_listen_for_connections_on = TCP_CONNECTION_PORT_B;
-        
         try
         {
-            SameHostConnection conn_obj = new SameHostConnection();
-            PassReturnArg side_a = PassReturnArg.external_create(
-                new RalphGlobals(params_a),conn_obj);
-            PassReturnArg side_b = PassReturnArg.external_create(
-                new RalphGlobals(params_b),conn_obj);
+            RalphGlobals.Parameters params_a = new RalphGlobals.Parameters();
+            params_a.tcp_port_to_listen_for_connections_on = TCP_CONNECTION_PORT_A;
+            RalphGlobals globals_a = new RalphGlobals(params_a);
+
+            RalphGlobals.Parameters params_b = new RalphGlobals.Parameters();
+            params_b.tcp_port_to_listen_for_connections_on = TCP_CONNECTION_PORT_B;
+            RalphGlobals globals_b = new RalphGlobals(params_b);
+
+            // connect hosts a and b, via a tcp connection
+            Thread.sleep(500);
+            Ralph.tcp_connect("127.0.0.1", TCP_CONNECTION_PORT_B, globals_a);
+            Thread.sleep(500);
+
+            // Generate an Endpoint that we will then install a remote PassReturnArg on
+            PassReturnArg side_a = PassReturnArg.external_create(globals_a);
+
+            InternalServiceFactory service_receiver_factory_to_send =
+                new InternalServiceFactory(
+                    PassReturnArg.factory, globals_a);
+            side_a.install_remote_pass_return_arg(service_receiver_factory_to_send);
+
 
             Double[] list_first_nums = new Double [] {1.0, 2.0, 3.5,5.5};
             Double[] list_second_nums = new Double [] {9.0, 2.2, 3.5,.5};
@@ -40,7 +51,7 @@ public class PartnerCallReturn
             {
                 Double num1 = list_first_nums[i];
                 Double num2 = list_second_nums[i];
-                            
+
                 Double result = side_a.pass_sum_return(num1, num2);
                 if (! result.equals(num1 + num2))
                     return false;
