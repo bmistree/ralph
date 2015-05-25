bbe50f6d67b3d4645343ea0d1bf020c0b2eb6bd2
Behram Mistree <behrammistree@forwardnetworks.com>
2015-05-25 16:29:57 -0700

    Updated serialize number list test to use new install syntax.

--- bin/emit_test/emit_test_harnesses/SerializeNumberListTest.java
+++ bin/emit_test/emit_test_harnesses/SerializeNumberListTest.java
@@ -1,14 +1,15 @@
 package emit_test_harnesses;
 
 import ralph_emitted.SerializeNumberListJava.ListSerializer;
-import RalphConnObj.SameHostConnection;
 import ralph.RalphGlobals;
+import ralph.InternalServiceFactory;
+import ralph.Ralph;
 
 public class SerializeNumberListTest
 {
     private final static int TCP_CONNECTION_PORT_A = 20494;
     private final static int TCP_CONNECTION_PORT_B = 20495;
-    
+
     public static void main(String[] args)
     {
         if (run_test())
@@ -21,18 +22,27 @@ public class SerializeNumberListTest
     {
         RalphGlobals.Parameters params_a = new RalphGlobals.Parameters();
         params_a.tcp_port_to_listen_for_connections_on = TCP_CONNECTION_PORT_A;
-        
+
         RalphGlobals.Parameters params_b = new RalphGlobals.Parameters();
         params_b.tcp_port_to_listen_for_connections_on = TCP_CONNECTION_PORT_B;
 
-        
+
         try
         {
-            SameHostConnection conn_obj = new SameHostConnection();
-            ListSerializer side_a = ListSerializer.external_create(
-                new RalphGlobals(params_a),conn_obj);
-            ListSerializer side_b = ListSerializer.external_create(
-                new RalphGlobals(params_b),conn_obj);
+            RalphGlobals globals_a = new RalphGlobals(params_a);
+            RalphGlobals globals_b = new RalphGlobals(params_b);
+
+            // connect hosts a and b, via a tcp connection
+            Thread.sleep(500);
+            Ralph.tcp_connect("127.0.0.1", TCP_CONNECTION_PORT_B, globals_a);
+            Thread.sleep(500);
+
+
+            ListSerializer side_a = ListSerializer.external_create(globals_a);
+            InternalServiceFactory service_receiver_factory_to_send =
+                new InternalServiceFactory(
+                    ListSerializer.factory, globals_a);
+            side_a.install_partner(service_receiver_factory_to_send);
 
             // tests atomic number list serialization
             if (! num_list_sum_test(true,side_a))
@@ -40,7 +50,7 @@ public class SerializeNumberListTest
             // tests non atomic number list serialization
             if (! num_list_sum_test(false,side_a))
                 return false;
-            
+
             return true;
         }
         catch(Exception _ex)
@@ -75,5 +85,5 @@ public class SerializeNumberListTest
         }
         return true;
     }
-    
+
 }
