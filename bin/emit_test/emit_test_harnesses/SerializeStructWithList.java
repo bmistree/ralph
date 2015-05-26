a02fb3fa1e9d41b5c669520dfdb2bfab95baa6dd
Behram Mistree <behrammistree@forwardnetworks.com>
2015-05-25 17:31:27 -0700

    Updated another serialize struct with list test to use new install syntax.

--- bin/emit_test/emit_test_harnesses/SerializeStructWithList.java
+++ bin/emit_test/emit_test_harnesses/SerializeStructWithList.java
@@ -1,9 +1,9 @@
 package emit_test_harnesses;
 
-
 import ralph_emitted.SerializeStructWithListJava.ListSerializer;
-import RalphConnObj.SameHostConnection;
 import ralph.RalphGlobals;
+import ralph.InternalServiceFactory;
+import ralph.Ralph;
 
 import static emit_test_harnesses.SerializeStructHarnessHelper.num_sum_test;
 
@@ -12,7 +12,7 @@ public class SerializeStructWithList
     // Using inexact check for floating point equality between doubles
     private final static int TCP_CONNECTION_PORT_A = 20494;
     private final static int TCP_CONNECTION_PORT_B = 20495;
-    
+
     public static void main(String[] args)
     {
         if (run_test())
@@ -25,21 +25,32 @@ public class SerializeStructWithList
     {
         RalphGlobals.Parameters params_a = new RalphGlobals.Parameters();
         params_a.tcp_port_to_listen_for_connections_on = TCP_CONNECTION_PORT_A;
-        
+
         RalphGlobals.Parameters params_b = new RalphGlobals.Parameters();
         params_b.tcp_port_to_listen_for_connections_on = TCP_CONNECTION_PORT_B;
-        
+
         try
         {
-            SameHostConnection conn_obj = new SameHostConnection();
-            ListSerializer side_a = ListSerializer.external_create(
-                new RalphGlobals(params_a),conn_obj);
-            ListSerializer side_b = ListSerializer.external_create(
-                new RalphGlobals(params_b),conn_obj);
+
+            RalphGlobals globals_a = new RalphGlobals(params_a);
+            RalphGlobals globals_b = new RalphGlobals(params_b);
+
+            // connect hosts a and b, via a tcp connection
+            Thread.sleep(500);
+            Ralph.tcp_connect("127.0.0.1", TCP_CONNECTION_PORT_B, globals_a);
+            Thread.sleep(500);
+
+            // Instantiate ListSerializer and have it build a remote copy
+            ListSerializer side_a = ListSerializer.external_create(globals_a);
+            InternalServiceFactory service_receiver_factory_to_send =
+                new InternalServiceFactory(
+                    ListSerializer.factory, globals_a);
+            side_a.install_partner(service_receiver_factory_to_send);
+
 
             if (! num_sum_test(side_a))
                 return false;
-            
+
             return true;
         }
         catch(Exception _ex)
@@ -48,5 +59,5 @@ public class SerializeStructWithList
             return false;
         }
     }
-    
+
 }
