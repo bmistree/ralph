f265247bae65f740f2de33b3d156001273a3af0a
Behram Mistree <behrammistree@forwardnetworks.com>
2015-05-25 17:26:54 -0700

    Updated another serialize struct with map test to use new install syntax.

--- bin/emit_test/emit_test_harnesses/SerializeStructWithMap.java
+++ bin/emit_test/emit_test_harnesses/SerializeStructWithMap.java
@@ -1,15 +1,17 @@
 package emit_test_harnesses;
 
 import ralph_emitted.SerializeStructWithMapJava.MapSerializer;
-import RalphConnObj.SameHostConnection;
 import ralph.RalphGlobals;
+import ralph.InternalServiceFactory;
+import ralph.Ralph;
+
 import static emit_test_harnesses.SerializeStructHarnessHelper.num_sum_test;
 
 public class SerializeStructWithMap
 {
     private final static int TCP_CONNECTION_PORT_A = 20494;
     private final static int TCP_CONNECTION_PORT_B = 20495;
-    
+
     public static void main(String[] args)
     {
         if (run_test())
@@ -22,21 +24,31 @@ public class SerializeStructWithMap
     {
         RalphGlobals.Parameters params_a = new RalphGlobals.Parameters();
         params_a.tcp_port_to_listen_for_connections_on = TCP_CONNECTION_PORT_A;
-        
+
         RalphGlobals.Parameters params_b = new RalphGlobals.Parameters();
         params_b.tcp_port_to_listen_for_connections_on = TCP_CONNECTION_PORT_B;
 
         try
         {
-            SameHostConnection conn_obj = new SameHostConnection();
-            MapSerializer side_a = MapSerializer.external_create(
-                new RalphGlobals(params_a),conn_obj);
-            MapSerializer side_b = MapSerializer.external_create(
-                new RalphGlobals(params_b),conn_obj);
+            RalphGlobals globals_a = new RalphGlobals(params_a);
+            RalphGlobals globals_b = new RalphGlobals(params_b);
+
+            // connect hosts a and b, via a tcp connection
+            Thread.sleep(500);
+            Ralph.tcp_connect("127.0.0.1", TCP_CONNECTION_PORT_B, globals_a);
+            Thread.sleep(500);
+
+
+            // Instantiate MapSerializer and have it build a remote copy
+            MapSerializer side_a = MapSerializer.external_create(globals_a);
+            InternalServiceFactory service_receiver_factory_to_send =
+                new InternalServiceFactory(
+                    MapSerializer.factory, globals_a);
+            side_a.install_partner(service_receiver_factory_to_send);
 
             if (! num_sum_test(side_a))
                 return false;
-            
+
             return true;
         }
         catch(Exception _ex)
