a8dfa622cbd622613f539750a3cfb93debe1670a
Behram Mistree <behrammistree@forwardnetworks.com>
2015-05-25 19:03:54 -0700

    Started to update durability test with new install syntax.  Test fails.  Must fix.

--- bin/emit_test/emit_test_harnesses/DurabilityReplayRPC.java
+++ bin/emit_test/emit_test_harnesses/DurabilityReplayRPC.java
@@ -7,17 +7,17 @@ import java.util.Random;
 import ralph_emitted.DurabilityReplayRPCJava.NumHolder;
 
 import RalphDurability.DurabilityReplayer;
-import RalphConnObj.SameHostConnection;
 import ralph.RalphGlobals;
 import ralph.DurabilityInfo;
-
+import ralph.InternalServiceFactory;
+import ralph.Ralph;
 
 public class DurabilityReplayRPC
 {
     public final static int MAX_NUM_RPCS_PER_TRANSACTION = 20;
     public final static int TCP_CONNECTION_PORT_A = 35855;
     public final static int TCP_CONNECTION_PORT_B = 35856;
-    
+
     public static void main(String[] args)
     {
         if (run_test())
@@ -33,17 +33,22 @@ public class DurabilityReplayRPC
         {
             RalphGlobals.Parameters params_a = new RalphGlobals.Parameters();
             params_a.tcp_port_to_listen_for_connections_on = TCP_CONNECTION_PORT_A;
-            RalphGlobals ralph_globals_a = new RalphGlobals(params_a);
-            
+            RalphGlobals globals_a = new RalphGlobals(params_a);
+
             RalphGlobals.Parameters params_b = new RalphGlobals.Parameters();
             params_b.tcp_port_to_listen_for_connections_on = TCP_CONNECTION_PORT_B;
-            RalphGlobals ralph_globals_b = new RalphGlobals(params_b);
-            
-            SameHostConnection conn_obj = new SameHostConnection();
-            NumHolder original_calling_on =
-                NumHolder.external_create(ralph_globals_a,conn_obj);
-            NumHolder original_called_on =
-                NumHolder.external_create(ralph_globals_b,conn_obj);
+            RalphGlobals globals_b = new RalphGlobals(params_b);
+
+            // connect hosts a and b, via a tcp connection
+            Thread.sleep(500);
+            Ralph.tcp_connect("127.0.0.1", TCP_CONNECTION_PORT_B, globals_a);
+            Thread.sleep(500);
+
+            // Instantiate NumHolder and have it build a remote copy
+            NumHolder original_calling_on = NumHolder.external_create(globals_a);
+            InternalServiceFactory service_receiver_factory_to_send =
+                new InternalServiceFactory(NumHolder.factory, globals_a);
+            original_calling_on.install_partner(service_receiver_factory_to_send);
 
             for (int i = 0; i < 20; ++i)
             {
@@ -52,45 +57,26 @@ public class DurabilityReplayRPC
                 original_calling_on.make_request(
                     new Double(rpcs_per_transaction));
             }
-            
+
             // Now, try to replay from durability file
             DurabilityReplayer replayer =
                 (DurabilityReplayer)DurabilityInfo.instance.durability_replayer;
 
-            while(replayer.step(ralph_globals_a)){}
+            while(replayer.step(globals_a)){}
 
-            // check that final internal values of both endpoints are
-            // correct.
+            // check that final internal value of endpoint is correct.
+            NumHolder replayed_calling_on =
+                (NumHolder)replayer.get_endpoint_if_exists(
+                    original_calling_on.uuid());
 
-            // first, checking rpc caller
-            {
-                NumHolder replayed_calling_on =
-                    (NumHolder)replayer.get_endpoint_if_exists(
-                        original_calling_on.uuid());
-                
-                Double replayed_internal =
-                    replayed_calling_on.get_internal_num();
-                Double original_internal =
-                    original_calling_on.get_internal_num();
-            
-                if (! replayed_internal.equals(original_internal))
-                    return false;
-            }
-            
-            // second, checking rpc recipient.
-            {
-                NumHolder replayed_called_on =
-                    (NumHolder)replayer.get_endpoint_if_exists(
-                        original_called_on.uuid());
-                
-                Double replayed_internal =
-                    replayed_called_on.get_internal_num();
-                Double original_internal =
-                    original_called_on.get_internal_num();
-                
-                if (! replayed_internal.equals(original_internal))
-                    return false;
-            }
+            Double replayed_internal =
+                replayed_calling_on.get_internal_num();
+            Double original_internal =
+                original_calling_on.get_internal_num();
+
+            if (! replayed_internal.equals(original_internal))
+                return false;
+
             return true;
         }
         catch(Exception _ex)
