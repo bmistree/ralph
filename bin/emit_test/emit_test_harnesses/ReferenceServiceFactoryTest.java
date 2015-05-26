ab49fccc954d8c51fb3529d50378783348877861
Behram Mistree <behrammistree@forwardnetworks.com>
2015-05-25 18:04:51 -0700

    Updated reference service factory test to use new install syntax.

--- bin/emit_test/emit_test_harnesses/ReferenceServiceFactoryTest.java
+++ bin/emit_test/emit_test_harnesses/ReferenceServiceFactoryTest.java
@@ -2,13 +2,11 @@ package emit_test_harnesses;
 
 import ralph_emitted.ReferenceServiceFactoryJava.Sender;
 import ralph_emitted.ReferenceServiceFactoryJava.Receiver;
-import ralph_emitted.ReferenceServiceFactoryJava.SenderCreated;
 import ralph_emitted.ReferenceServiceFactoryJava.ReceiverCreated;
-import RalphConnObj.SameHostConnection;
 import ralph.RalphGlobals;
 import ralph.InternalServiceFactory;
 import ralph.EndpointConstructorObj;
-
+import ralph.Ralph;
 
 public class ReferenceServiceFactoryTest
 {
@@ -29,43 +27,43 @@ public class ReferenceServiceFactoryTest
             new RalphGlobals.Parameters();
         params_receiver.tcp_port_to_listen_for_connections_on =
             TCP_CONNECTION_PORT_RECEIVER;
-        
+
         RalphGlobals.Parameters params_sender =
             new RalphGlobals.Parameters();
         params_sender.tcp_port_to_listen_for_connections_on =
             TCP_CONNECTION_PORT_SENDER;
 
-        
         try
         {
-            SameHostConnection conn_obj = new SameHostConnection();
-            RalphGlobals sender_globals =
-                new RalphGlobals(params_sender);
-            Sender sender = Sender.external_create(sender_globals,conn_obj);
-            Receiver receiver =
-                Receiver.external_create(
-                    new RalphGlobals(params_receiver),conn_obj);
+            RalphGlobals globals_receiver = new RalphGlobals(params_receiver);
+            RalphGlobals globals_sender = new RalphGlobals(params_sender);
+
+            // connect hosts a and b, via a tcp connection
+            Thread.sleep(500);
+            Ralph.tcp_connect("127.0.0.1", TCP_CONNECTION_PORT_RECEIVER, globals_sender);
+            Thread.sleep(500);
+
+            // Instantiate SerializeNull and have it build a remote copy
+            Sender sender = Sender.external_create(globals_sender);
+            InternalServiceFactory service_receiver_factory_to_send =
+                new InternalServiceFactory(
+                    Receiver.factory, globals_sender);
+            sender.install_partner(service_receiver_factory_to_send);
 
             EndpointConstructorObj receiver_creater =
                 ReceiverCreated.factory;
-            EndpointConstructorObj sender_creater =
-                SenderCreated.factory;
 
             // service factories are instantiated on sender, rather
             // than receiver (ie, use sender_globals here)
             InternalServiceFactory receiver_creater_service_factory =
-                new InternalServiceFactory(receiver_creater,sender_globals);
-            InternalServiceFactory sender_creater_service_factory =
-                new InternalServiceFactory(sender_creater,sender_globals);
-            
+                new InternalServiceFactory(receiver_creater, globals_sender);
+
             Double result =
-                sender.instantiate_remote(
-                    receiver_creater_service_factory,
-                    sender_creater_service_factory);
+                sender.instantiate_remote(receiver_creater_service_factory);
+            
             if (!result.equals(1.0))
                 return false;
-            
-                        
+
             return true;
         }
         catch(Exception _ex)
