995bc3022ef4d09576c00eec8f525fe63d8190c0
Behram Mistree <behrammistree@forwardnetworks.com>
2015-05-31 18:43:10 -0700

    Updated recursive partner test to use new install syntax.

--- bin/emit_test/emit_test_harnesses/RecursivePartnerAddition.java
+++ bin/emit_test/emit_test_harnesses/RecursivePartnerAddition.java
@@ -1,14 +1,15 @@
 package emit_test_harnesses;
 
 import ralph_emitted.RecursivePartnerAdditionJava.RecursiveAdder;
-import RalphConnObj.SameHostConnection;
 import ralph.RalphGlobals;
+import ralph.Ralph;
+import ralph.InternalServiceFactory;
 
 public class RecursivePartnerAddition
 {
     private final static int TCP_CONNECTION_PORT_A = 20494;
     private final static int TCP_CONNECTION_PORT_B = 20495;
-    
+
     public static void main(String[] args)
     {
         if (run_test())
@@ -21,39 +22,44 @@ public class RecursivePartnerAddition
     {
         RalphGlobals.Parameters params_a = new RalphGlobals.Parameters();
         params_a.tcp_port_to_listen_for_connections_on = TCP_CONNECTION_PORT_A;
-        
+
         RalphGlobals.Parameters params_b = new RalphGlobals.Parameters();
         params_b.tcp_port_to_listen_for_connections_on = TCP_CONNECTION_PORT_B;
-        
+
         try
         {
-            SameHostConnection conn_obj = new SameHostConnection();
-            RecursiveAdder side_a = RecursiveAdder.external_create(
-                new RalphGlobals(params_a),conn_obj);
-            RecursiveAdder side_b = RecursiveAdder.external_create(
-                new RalphGlobals(params_b),conn_obj);
+            RalphGlobals globals_a = new RalphGlobals(params_a);
+            RalphGlobals globals_b = new RalphGlobals(params_b);
+
+            Ralph.tcp_connect("127.0.0.1", TCP_CONNECTION_PORT_A,
+                              globals_b);
+
+            // wait for everything to settle down
+            Thread.sleep(1000);
+
+            RecursiveAdder side_a = RecursiveAdder.external_create(globals_a);
+            InternalServiceFactory receiver_factory =
+                new InternalServiceFactory(RecursiveAdder.factory, globals_a);
+            side_a.install_partner(receiver_factory);
 
             Double[] list_first_nums = new Double [] {1.0, 2.0, 7.,5.5};
             Double[] list_second_nums = new Double [] {9.0, 2.2, 3.5,.5};
             Double[] num_times_to_run = new Double [] {10., 15., 3., 4., 5.};
-            
+
             for (int i = 0; i < list_first_nums.length; ++i)
             {
                 Double first_num = list_first_nums[i];
-                
                 for (int j = 0; j < list_second_nums.length; ++j)
                 {
                     Double second_num = list_second_nums[i];
-
                     for (int k = 0; k < num_times_to_run.length; ++k)
                     {
                         Double num_to_run = num_times_to_run[k];
 
                         boolean test_passed =
-                            run_single_test(
-                                side_a,side_b,first_num,second_num,
-                                num_to_run);
-                                            
+                            run_single_test(side_a, first_num, second_num,
+                                            num_to_run);
+
                         if (! test_passed)
                             return false;
                     }
@@ -72,16 +78,16 @@ public class RecursivePartnerAddition
        @return true if test passes, false if fails.
      */
     public static boolean run_single_test(
-        RecursiveAdder side_a, RecursiveAdder side_b, Double internal_a,
-        Double internal_b, Double num_iters) throws Exception
+        RecursiveAdder side_a, Double internal_a, Double internal_b,
+        Double num_iters) throws Exception
     {
         Double expected_result =
             internal_a*Math.ceil(num_iters/2.) +
             internal_b*Math.floor(num_iters/2.);
 
         side_a.set_internal_num(internal_a);
-        side_b.set_internal_num(internal_b);
-        
+        side_a.set_remote_internal_num(internal_b);
+
         Double result = side_a.partner_recursive_add_to_number(num_iters);
 
         boolean to_return = double_check_equals(result,expected_result);
