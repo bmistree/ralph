f1038efd52c2065044b9d15b8026bc4c7a7494ff
bmistree <bmistree@gmail.com>
2015-08-23 13:52:19 -0700

    Updated replay struct test to use new install syntax.

--- bin/emit_test/emit_test_harnesses/ReplayPartnerStructElement.java
+++ bin/emit_test/emit_test_harnesses/ReplayPartnerStructElement.java
@@ -8,9 +8,7 @@ import ralph.RalphObject;
 import ralph.RalphGlobals;
 import ralph.VersioningInfo;
 import ralph.Endpoint;
-import ralph.EndpointConstructorObj;
-
-import RalphConnObj.TCPConnectionObj;
+import ralph.InternalServiceFactory;
 
 import RalphVersions.VersionUtil;
 import RalphVersions.ReconstructionContext;
@@ -28,18 +26,13 @@ public class ReplayPartnerStructElement
     private static final String HOST_NAME = "localhost";
     private static final int TCP_LISTENING_PORT = 38689;
 
-    private static final ReceiverConstructor RECEIVER_CONSTRUCTOR =
-        new ReceiverConstructor();
-    
-    private static StructSender sender = null;
-    private static StructReceiver receiver = null;
     private final static int TCP_CONNECTION_PORT_SENDER = 20494;
     private final static int TCP_CONNECTION_PORT_RECEIVER = 20495;
     private final static int TCP_CONNECTION_PORT_RECONSTRUCTOR = 20496;
-    
+
     private final static int LAST_NUMBER_UPDATED = 100;
-    
-    
+
+
     public static void main(String[] args)
     {
         if (run_test())
@@ -64,39 +57,37 @@ public class ReplayPartnerStructElement
             new RalphGlobals.Parameters();
         params_reconstructor.tcp_port_to_listen_for_connections_on =
             TCP_CONNECTION_PORT_RECONSTRUCTOR;
-        
+
         try
         {
-            // populates receiver
-            Ralph.tcp_accept(
-                RECEIVER_CONSTRUCTOR, HOST_NAME, TCP_LISTENING_PORT,
-                new RalphGlobals(params_receiver));
+            RalphGlobals ralph_globals_receiver =
+                new RalphGlobals(params_receiver);
+            RalphGlobals ralph_globals_sender =
+                new RalphGlobals(params_sender);
 
-
-            // wait for the other side to ensure that it's listening
-            Thread.sleep(1000);
-            try
-            {
-                sender = (StructSender)Ralph.tcp_connect(
-                    StructSender.factory, HOST_NAME, TCP_LISTENING_PORT,
-                    new RalphGlobals(params_sender));
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-                assert(false);
-            }
+            Ralph.tcp_connect("127.0.0.1", TCP_CONNECTION_PORT_SENDER,
+                              ralph_globals_receiver);
 
             // wait for everything to settle down
             Thread.sleep(1000);
+
+            StructSender sender =
+                StructSender.create_single_sided(ralph_globals_sender);
+            InternalServiceFactory receiver_factory =
+                new InternalServiceFactory(
+                    StructReceiver.factory, ralph_globals_receiver);
+            sender.install_partner(receiver_factory);
+
 
             for (int i = 0; i <= LAST_NUMBER_UPDATED; ++i)
                 sender.send_struct((double) i);
 
             // double check that number on receiver is what we'd expect:
-            if (!perform_correctness_checks(receiver))
+            if (!perform_correctness_checks(sender))
                 return false;
-                        
+
+            String replayed_endpt_uuid = sender.get_remote_endpt().service_uuid;
+
             VersioningInfo.instance.version_saver.flush();
 
             // Now replay
@@ -107,16 +98,16 @@ public class ReplayPartnerStructElement
                     VersioningInfo.instance.version_replayer,
                     ralph_globals_reconstructor);
 
-            // now, tries to replay changes to endpoint.  
+            // now, tries to replay changes to endpoint.
             StructReceiver replayed_receiver =
                 (StructReceiver) VersionUtil.rebuild_endpoint(
-                    receiver._uuid,ralph_globals_reconstructor,
+                    replayed_endpt_uuid, ralph_globals_reconstructor,
                     reconstruction_context);
 
             // ensure replayed receiver has correct internal values.
             if (!perform_correctness_checks(replayed_receiver))
                 return false;
-            
+
             return true;
         }
         catch(Exception _ex)
@@ -126,41 +117,22 @@ public class ReplayPartnerStructElement
         }
     }
 
-    /**
-       Must override constructor so that can save global SideB after
-       constructing it.
-     */
-    private static class ReceiverConstructor implements EndpointConstructorObj
+    private static boolean perform_correctness_checks(StructSender sender)
+        throws Exception
     {
-        private final static String canonical_name =
-            ReceiverConstructor.class.getName();
-        
-        @Override
-        public Endpoint construct(
-            RalphGlobals globals, RalphConnObj.ConnectionObj conn_obj,
-            IDurabilityContext durability_context,
-            DurabilityReplayContext durability_replay_context)
-        {
-            receiver =
-                (StructReceiver) StructReceiver.factory.construct(
-                    globals,conn_obj,durability_context,
-                    durability_replay_context);
-            return receiver;
-        }
-        @Override
-        public Endpoint construct(
-            RalphGlobals globals, RalphConnObj.ConnectionObj conn_obj,
-            List<RalphObject> internal_val_list,
-            IDurabilityContext durability_context)
-        {
-            return construct(globals,conn_obj,durability_context,null);
-        }
+        int size_of_struct_list = sender.struct_list_size().intValue();
+        // note +1 because we performed this many sets.
+        if (size_of_struct_list != (LAST_NUMBER_UPDATED +1))
+            return false;
 
-        @Override
-        public String get_canonical_name()
+        for (int i = 0; i <= LAST_NUMBER_UPDATED; ++i)
         {
-            return canonical_name;
+            int internal_struct_value =
+                sender.struct_list_internal_val((double)i).intValue();
+            if (i != internal_struct_value)
+                return false;
         }
+        return true;
     }
 
     private static boolean perform_correctness_checks(StructReceiver receiver)
@@ -180,5 +152,4 @@ public class ReplayPartnerStructElement
         }
         return true;
     }
-
 }
